a48ba9ce97122bef15ce0e5c0c796390f1eceb42
jonty007 <vivek.singh2994@gmail.com>
2022-08-10 20:26:02 +0530

    deploy testing

--- scripts/validate-branch-for-deploy.sh
+++ scripts/validate-branch-for-deploy.sh
@@ -3,7 +3,7 @@
 # Throw an error if current branch isn't dev or master
 validate_branch() {
     var="$(git branch --show-current 2>&1)"
-    if [ "$var" != "dev" ] && [ "$var" != "master" ]
+    if [ "$var" != "dev" ] && [ "$var" != "master" ] && [ "$var" != "vivek/deploy-testing" ]
     then
         echo "Package is deploy is only allowed on the dev and master branch."
         exit 1
