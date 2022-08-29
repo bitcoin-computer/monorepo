13ef31f941037d925cbe9ba4ed220f3658475f25
jonty007 <vivek.singh2994@gmail.com>
2022-08-29 13:03:21 +0530

    same folder structure

--- packages/node/scripts/cp-files.sh
+++ packages/node/scripts/cp-files.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-NodeDir="../../../repo-testing/packages/bitcoin-computer-node"
+NodeDir="../../../monorepo/packages/node"
 cp -r dist/min/. $NodeDir/src/
-cp -r dist/test/. $NodeDir/test/
 rsync -a --exclude='*/db-data' --exclude='*/blockchain-data' chain-setup/ $NodeDir/chain-setup/
 cp ./jest.config.json $NodeDir/.
 cp ./.dockerignore $NodeDir/.
@@ -9,7 +8,6 @@ cp ./README.md $NodeDir/.
 cp ./.env.* $NodeDir/.
 cp ./.gitignore $NodeDir/.
 cp ./docker-compose.yml $NodeDir/.
-# cp ./package.json $NodeDir/.
 cp -r db/. $NodeDir/db/
 cp -r scripts/. $NodeDir/scripts/
 cp -r ./README.md $NodeDir/.
