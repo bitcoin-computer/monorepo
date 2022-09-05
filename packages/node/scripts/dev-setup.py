2759117038b45848a1a096caf765979e798cb5ca
ltardivo <lauratardivo@gmail.com>
2022-09-05 15:11:21 -0300

    Deploy v0.10.0-alpha.3

--- packages/node/scripts/dev-setup.py
+++ packages/node/scripts/dev-setup.py
@@ -23,10 +23,8 @@ args = parser.parse_args()
 print(args)
 
 port = '8332' if args.chain == 'BTC' else '19332'
-testing = 'true'
-command = "BCN_URL=http://127.0.0.1:3000 BCN_ENV=dev CHAIN={0} NETWORK={1} POSTGRES_HOST=127.0.0.1 RPC_HOST=127.0.0.1 RPC_PORT={2}  RPC_PROTOCOL=http RPC_USER=bcn-admin RPC_PASSWORD=kH4nU5Okm6-uyC0_mA5ztVNacJqZbYd_KGLl6mx722A= ZMQ_URL=tcp://127.0.0.1:28332 UN_P2SH_URL=http://127.0.0.1:3000 TESTING={3} node --loader ts-node/esm $(grep START_PATH .package.paths | cut -d '=' -f2)".format(args.chain, args.network, port, testing)
+command = "BCN_URL=http://127.0.0.1:3000 BCN_ENV=dev CHAIN={0} NETWORK={1} POSTGRES_HOST=127.0.0.1 RPC_HOST=127.0.0.1 RPC_PORT={2}  RPC_PROTOCOL=http RPC_USER=bcn-admin RPC_PASSWORD=kH4nU5Okm6-uyC0_mA5ztVNacJqZbYd_KGLl6mx722A= ZMQ_URL=tcp://127.0.0.1:28332 UN_P2SH_URL=http://127.0.0.1:3000 node --loader ts-node/esm $(grep START_PATH .package.paths | cut -d '=' -f2)".format(args.chain, args.network, port)
 
-print('Attention: TESTING == {0}'.format(testing))
 print(command)
 
 # ltc and regtest are the default
