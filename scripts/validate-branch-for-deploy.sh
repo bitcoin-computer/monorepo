2430d584fb885d233d016c1e410b4a02510258fe
ltardivo <lauratardivo@gmail.com>
2022-09-08 19:41:05 -0300

    Modifies validate-branch script. Includes 'main' as a valid branch for publishing to NPM.

--- scripts/validate-branch-for-deploy.sh
+++ scripts/validate-branch-for-deploy.sh
@@ -3,9 +3,9 @@
 # Throw an error if current branch isn't dev or master
 validate_branch() {
     var="$(git branch --show-current 2>&1)"
-    if [ "$var" != "dev" ] && [ "$var" != "master" ]
+    if [ "$var" != "dev" ] && [ "$var" != "master" ] && [ "$var" != "main" ]
     then
-        echo "Package is deploy is only allowed on the dev and master branch."
+        echo "Package is deploy is only allowed on dev, master or main branches."
         exit 1
     fi
 }
