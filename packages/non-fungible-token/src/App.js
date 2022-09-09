7e62f387ab053aa8bc50aa4a4e1c1019b30c70e3
jonty007 <vivek.singh2994@gmail.com>
2022-09-09 15:22:08 +0530

    upgraded nft package to latest

--- packages/non-fungible-token/src/App.js
+++ packages/non-fungible-token/src/App.js
@@ -1,31 +1,32 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Computer } from 'bitcoin-computer-lib';
-import './App.css';
-import Card from './card';
-import Artwork from './artwork';
-import { areEqual } from './util';
+import React, { useState, useEffect, useRef } from "react";
+import { Computer } from "@bitcoin-computer/lib";
+import "./App.css";
+import Card from "./card";
+import Artwork from "./artwork";
+import { areEqual } from "./util";
 
 function App() {
   const [config] = useState({
-    chain: 'LTC',
-    network: 'testnet',
-    url: 'https://node.bitcoincomputer.io',
+    chain: "LTC",
+    // network: "testnet",
+    // url: "https://node.bitcoincomputer.io",
     // to run locally, change network and url:
-    // network: 'regtest',
-    // url: 'http://127.0.0.1:3000',
+    network: "regtest",
+    url: "http://127.0.0.1:3000",
   });
   const [computer, setComputer] = useState(
     new Computer({
       ...config,
-      seed: 'invest vacuum fee rookie door tool surround side embark squirrel symptom fit'
+      mnemonic:
+        "travel upgrade inside soda birth essence junk merit never twenty system opinion",
     })
   );
 
   const [balance, setBalance] = useState(0);
 
-  const [title, setTitle] = useState('');
-  const [artist, setArtist] = useState('');
-  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [url, setUrl] = useState("");
 
   const [revs, setRevs] = useState([]);
   const [artworks, setArtworks] = useState([]);
@@ -51,79 +52,78 @@ function App() {
   }
 
   useEffect(() => {
-    console.log('new address generated');
+    console.log("new address generated");
   }, [computer]);
 
   useInterval(async () => {
     try {
       if (computer) {
-        const newBalance = await computer.db.wallet.getBalance();
-        const newRevs = await computer.getRevs(
-          computer.db.wallet.getPublicKey()
-        );
-        console.log('public key: ', computer.db.wallet.getPublicKey());
-        console.log('revs', newRevs);
+        const newBalance = await computer.getBalance();
+        const newRevs = await computer.getRevs(computer.getPublicKey());
+        console.log("public key: ", computer.getPublicKey());
+        console.log("revs", newRevs);
         // sync art work when revs are not same
         if (!areEqual(revs, newRevs)) {
           const newArts = await Promise.all(
             newRevs.map(async (rev) => computer.sync(rev))
           );
-          console.log('artworks', newArts);
+          console.log("artworks", newArts);
           setArtworks(newArts);
         } else {
-          console.log('no new art added');
+          console.log("no new art added");
         }
         setBalance(newBalance);
         setRevs(newRevs);
       }
     } catch (err) {
-      console.log('error occurred while fetching wallet details: ', err);
+      console.log("error occurred while fetching wallet details: ", err);
     }
   }, 10000);
 
   const artSendingInProgress = (flag) => {
-    console.log('called from child')
+    console.log("called from child");
   };
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     try {
       if (!title) {
-        alert('Provide valid title.');
+        alert("Provide valid title.");
         return;
       }
       if (!artist) {
-        alert('Provide valid artist.');
+        alert("Provide valid artist.");
         return;
       }
       if (!url) {
-        alert('Provide valid url.');
+        alert("Provide valid url.");
         return;
       }
       const artwork = await computer.new(Artwork, [title, artist, url]);
-      console.log('created artwork', artwork);
+      console.log("created artwork", artwork);
     } catch (err) {
-      console.log('error occurred while creating art: ', err);
+      console.log("error occurred while creating art: ", err);
     } finally {
-      setTitle('')
-      setArtist('')
-      setUrl('')
+      setTitle("");
+      setArtist("");
+      setUrl("");
     }
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       {
         <div>
           <h2>Wallet</h2>
-          <b>Address</b>&nbsp;{computer.db.wallet.getAddress().toString()}
+          <b>Address</b>&nbsp;{computer.getAddress().toString()}
           <br />
-          <b>Public Key</b>&nbsp;{computer.db.wallet.getPublicKey().toString()}
+          <b>Public Key</b>&nbsp;{computer.getPublicKey().toString()}
           <br />
-          <b>Balance</b>&nbsp;{balance / 1e8}{' LTC'}
+          <b>Balance</b>&nbsp;{balance / 1e8}
+          {" LTC"}
           <br />
           <button
-            type='submit'
+            type="submit"
             onClick={() => setComputer(new Computer(config))}
           >
             Generate New Wallet
@@ -133,34 +133,38 @@ function App() {
             Title
             <br />
             <input
-              type='string'
+              type="string"
               value={title}
               onChange={(e) => setTitle(e.target.value)}
             />
             Artist
             <br />
             <input
-              type='string'
+              type="string"
               value={artist}
               onChange={(e) => setArtist(e.target.value)}
             />
             Url
             <br />
             <input
-              type='string'
+              type="string"
               value={url}
               onChange={(e) => setUrl(e.target.value)}
             />
-            <button type='submit' value='Send Bitcoin'>
+            <button type="submit" value="Send Bitcoin">
               Create Artwork
             </button>
           </form>
           <h2>Your Artworks</h2>
-          <ul className='flex-container'>
+          <ul className="flex-container">
             {artworks.map((artwork) => (
-              <Card artwork={artwork} setArtSending={artSendingInProgress} key={artwork.url}/>
+              <Card
+                artwork={artwork}
+                setArtSending={artSendingInProgress}
+                key={artwork.url}
+              />
             ))}
-          </ul>{' '}
+          </ul>{" "}
         </div>
       }
     </div>
