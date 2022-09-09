75df8ec99519a7376aa065342be9ffcb7226d6d7
jonty007 <vivek.singh2994@gmail.com>
2022-09-09 15:34:55 +0530

    react litecoin wallet upgrade

--- packages/react-bitcoin-wallet/src/App.test.js
+++ packages/react-bitcoin-wallet/src/App.test.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  const root = ReactDOM.createRoot(div);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
