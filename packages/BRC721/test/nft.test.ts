78312a9601bac1afaf45f547a4454ba3e3e110b6
ltardivo <lauratardivo@gmail.com>
2022-09-12 11:41:53 -0300

    Update BRC721. Replaces Jest by Mocha

--- packages/BRC721/test/nft.test.ts
+++ packages/BRC721/test/nft.test.ts
@@ -1,72 +1,67 @@
-import { Computer } from 'bitcoin-computer-lib'
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai'
+import { Computer } from '@bitcoin-computer/lib'
 import { NFT } from '../src/nft'
 
 const randomOpts = {
-  // uncomment to run locally
   chain: 'LTC',
   url: 'http://127.0.0.1:3000',
   network: 'regtest',
 }
 
 const opts = {
-  seed: 'opera deputy attitude upset royal keep',
+  mnemonic: 'opera deputy attitude upset royal keep',
   ...randomOpts,
 }
 
-describe('TokenBad', () => {
+describe('NFT', () => {
   describe('Constructor', () => {
     it('should create a Javascript object', () => {
-      expect(NFT).toBeDefined()
-      expect(typeof NFT).toBe('function')
+      expect(NFT).not.to.be.undefined
+      expect(typeof NFT).to.eq('function')
 
       const token = new NFT('to', 'name', 'symbol')
-      expect(token).toBeDefined()
+      expect(token).not.to.be.undefined
     })
 
     it('should create a smart object', async () => {
       const computer = new Computer(opts)
-      const publicKeyString = computer.db.wallet.getPublicKey().toString()
+      const publicKeyString = computer.getPublicKey()
 
       const nft = await computer.new(NFT, [publicKeyString, 'name', 'symbol'])
-      expect(nft).toEqual({
-        name: 'name',
-        symbol: 'symbol',
-        _owners: [publicKeyString],
-        _id: expect.any(String),
-        _rev: expect.any(String),
-        _root: expect.any(String),
-      })
-    }, 20000)
+      expect(nft._owners).deep.equal([publicKeyString])
+      expect(nft.name).to.eq('name')
+      expect(nft.symbol).to.eq('symbol')
+      expect(nft._id).to.be.a('string')
+      expect(nft._rev).to.be.a('string')
+      expect(nft._root).to.be.a('string')
+    })
   })
 
   describe('transfer', () => {
     it('Should update a smart object', async () => {
       const computer = new Computer(opts)
-      const publicKeyString = computer.db.wallet.getPublicKey().toString()
+      const publicKeyString = computer.getPublicKey()
 
       const computer2 = new Computer(randomOpts)
-      const publicKeyString2 = computer2.db.wallet.getPublicKey().toString()
+      const publicKeyString2 = computer2.getPublicKey()
 
       const nft = await computer.new(NFT, [publicKeyString, 'name', 'symbol'])
-      expect(nft).toEqual({
-        name: 'name',
-        symbol: 'symbol',
-        _owners: [publicKeyString],
-        _id: expect.any(String),
-        _rev: expect.any(String),
-        _root: expect.any(String),
-      })
+      expect(nft._owners).deep.equal([publicKeyString])
+      expect(nft.name).to.eq('name')
+      expect(nft.symbol).to.eq('symbol')
+      expect(nft._id).to.be.a('string')
+      expect(nft._rev).to.be.a('string')
+      expect(nft._root).to.be.a('string')
 
       await nft.transfer(publicKeyString2, 1)
 
-      expect(nft).toEqual({
-        name: 'name',
-        symbol: 'symbol',
-        _owners: [publicKeyString2],
-        _id: expect.any(String),
-        _rev: expect.any(String),
-        _root: expect.any(String),
-      })
-    }, 20000)
+      expect(nft._owners).deep.equal([publicKeyString2])
+      expect(nft.name).to.eq('name')
+      expect(nft.symbol).to.eq('symbol')
+      expect(nft._id).to.be.a('string')
+      expect(nft._rev).to.be.a('string')
+      expect(nft._root).to.be.a('string')
+    })
   })
 })
