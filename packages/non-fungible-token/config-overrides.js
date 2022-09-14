eeffe2bc6ad8f53247f18309cb5dec0c33ebeff2
Clemens Ley <ley.clemens@gmail.com>
2022-09-14 16:31:26 -0700

    Fix webpack issue

--- packages/non-fungible-token/config-overrides.js
+++ packages/non-fungible-token/config-overrides.js
@@ -6,6 +6,10 @@ module.exports = function override(config) {
     crypto: require.resolve("crypto-browserify"),
     stream: require.resolve("stream-browserify"),
     assert: require.resolve("assert"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    os: require.resolve("os-browserify"),
+    url: require.resolve("url"),
     child_process: false,
   });
   config.resolve.fallback = fallback;
