a0dcd675d116e6b815c107a712d4849fe317138f
jonty007 <vivek.singh2994@gmail.com>
2022-10-10 21:35:05 +0530

    brc cleanup

--- packages/BRC721/src/brc721.ts
+++ packages/BRC721/src/brc721.ts
@@ -1,10 +1,10 @@
 import { NFT } from './nft'
 
 interface IBRC721 {
-  mint(to: string, name?: string, symbol?: string): Promise<NFT>
-  balanceOf(publicKey: string): Promise<number>
+  balanceOf(owner: string): Promise<number>
   ownerOf(tokenId: string): Promise<string[]>
-  transfer(to: string, tokenId: string)
+  transferFrom(to: string, tokenId: string)
+  mint(to: string, name?: string, symbol?: string): Promise<NFT>
 }
 
 export class BRC721 implements IBRC721 {
@@ -15,19 +15,8 @@ export class BRC721 implements IBRC721 {
     this.computer = computer
   }
 
-  async mint(to: string, name?: string, symbol?: string): Promise<NFT> {
-    if (!this.masterNFT && name && symbol) {
-      this.masterNFT = this.computer.new(NFT, [to, name, symbol])
-      return this.masterNFT
-    }
-    if (this.masterNFT.name !== name || this.masterNFT.symbol !== symbol) {
-      throw new Error('Name or symbol mismatch when minting token.')
-    }
-    return this.masterNFT.mint(to)
-  }
-
-  async balanceOf(publicKey: string): Promise<number> {
-    const revs = await this.computer.queryRevs({ publicKey, contract: NFT })
+  async balanceOf(owner: string): Promise<number> {
+    const revs = await this.computer.queryRevs({ owner, contract: NFT })
     const objects: NFT[] = await Promise.all(revs.map((rev) => this.computer.sync(rev)))
     objects.flatMap((object) => (object._root === this.masterNFT._root ? [object] : []))
     return objects.length
@@ -39,9 +28,20 @@ export class BRC721 implements IBRC721 {
     return obj._owners
   }
 
-  async transfer(to: string, tokenId: string) {
+  async transferFrom(to: string, tokenId: string) {
     const [rev] = await this.computer.getLatestRevs([tokenId])
     const obj = await this.computer.sync(rev)
     await obj.transfer(to)
+  }
+
+  async mint(to: string, name?: string, symbol?: string): Promise<NFT> {
+    if (!this.masterNFT && name && symbol) {
+      this.masterNFT = this.computer.new(NFT, [to, name, symbol])
+      return this.masterNFT
+    }
+    if (this.masterNFT.name !== name || this.masterNFT.symbol !== symbol) {
+      throw new Error('Name or symbol mismatch when minting token.')
+    }
+    return this.masterNFT.mint(to)
   }
 }
