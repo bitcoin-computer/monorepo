0c8dba64334513ad23fdfb581b4654c78e38055c
ltardivo <lauratardivo@gmail.com>
2022-12-19 19:34:18 -0300

    Uses require to import Contract from @bitcoin-computer/lib

--- packages/non-fungible-token/src/component/artworks/artwork.js
+++ packages/non-fungible-token/src/component/artworks/artwork.js
@@ -1,5 +1,8 @@
-export default class Artwork {
+const { Contract } = require('@bitcoin-computer/lib')
+
+export default class Artwork extends Contract {
   constructor(title, artist, url) {
+    super()
     this.title = title
     this.artist = artist
     this.url = url
