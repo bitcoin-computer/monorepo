d9706c5d1873d8dad25d82c93dbe2fad876a343c
ltardivo <lauratardivo@gmail.com>
2022-12-21 09:57:01 -0300

    Deploy 0.11.0-beta.3

--- packages/node/scripts/down.py
+++ packages/node/scripts/down.py
@@ -17,14 +17,6 @@ networkGroup.add_argument("-m", "--mainnet", action="store_const", dest="network
 networkGroup.add_argument("-r", "--regtest", action="store_const", dest="network", const='regtest')
 parser.set_defaults(network='regtest')
 
-
-cloudGroup = parser.add_mutually_exclusive_group()
-cloudGroup.add_argument("--aws", action="store_const", dest="cloud", const='aws')
-cloudGroup.add_argument("--local", action="store_const", dest="cloud", const='local')
-cloudGroup.add_argument('--convert', action="store_const", dest="cloud", const='convert')
-
-parser.set_defaults(cloud='local')
-
 serviceGroup = parser.add_mutually_exclusive_group()
 serviceGroup.add_argument('-db', action="store_const", dest="service", const='db')
 serviceGroup.add_argument('-bcn', action="store_const", dest="service", const='bcn')
@@ -39,5 +31,6 @@ print(args)
 
 commandLine = ' docker compose -f docker-compose.yml -f chain-setup/'+args.chain+'-'+args.network+'/docker-compose-local-'+args.chain+'-'+args.network+'.yml '
 
+print(commandLine+' down')
 subprocess.run(
-    ['sh', '-c', 'docker compose -f docker-compose.yml -f chain-setup/btc-testnet/docker-compose-local-btc-testnet.yml down'])
+    ['sh', '-c', commandLine+' down'])
