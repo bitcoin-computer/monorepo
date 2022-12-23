fc5971277e7196f84d7146c157770df2a02dc2d4
Clemens Ley <ley.clemens@gmail.com>
2022-12-23 16:48:34 +0100

    Minor

--- packages/chat/src/StartChat.js
+++ packages/chat/src/StartChat.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-// import ChatSc from "./chat-sc"
 
 function StartChat({ computer, Contract }) {
   class ChatSc extends Contract {
