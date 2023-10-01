0f289f48ca03a5e7add23dd5185732d6c209139b
Clemens Ley <ley.clemens@gmail.com>
2023-10-01 00:25:41 -0700

    Add company colors

--- packages/explorer/tailwind.config.js
+++ packages/explorer/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
-        "bit-blue": "#4284FB",
+        "blue-1": "#000F38",
+        "blue-2": "#002A99",
+        "blue-3": "#0046FF",
+        "blue-4": "#A7BFFF"
       },
     },
   },
