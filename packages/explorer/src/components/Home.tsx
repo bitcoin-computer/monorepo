bcdaac2374012e769f17f7a3c4e1788c9d42a48b
Clemens Ley <ley.clemens@gmail.com>
2023-10-01 00:25:30 -0700

    Start to cleanup homepage

--- packages/explorer/src/components/Home.tsx
+++ packages/explorer/src/components/Home.tsx
@@ -1,10 +1,9 @@
 import { Computer } from "@bitcoin-computer/lib"
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-import { BsArrowUpRight } from "react-icons/bs"
 import Loader from "./Loader"
 
-export default function SmartContracts(props: { computer: Computer }) {
+export default function Home(props: { computer: Computer }) {
   const { computer } = props
   const contractsPerPage = 7
 
@@ -52,21 +51,24 @@ export default function SmartContracts(props: { computer: Computer }) {
   }
   return (
     <div className="relative overflow-x-auto sm:rounded-lg pt-4">
-      <table className="w-full text-sm text-left text-gray-500 ">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+      <h1 className="mb-4 text-5xl font-extrabold dark:text-white">Bitcoin Computer Explorer</h1>
+
+      <h2 className="mb-2 text-4xl font-bold dark:text-white">Smart Contracts</h2>
+
+      <table className="w-full mt-4 mb-4 text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
-              Smart Contract
+              Output Identifier
             </th>
           </tr>
         </thead>
         <tbody>
           {revs.map((rev) => (
-            <tr key={rev} className="bg-white border-b hover:bg-gray-50 ">
+            <tr key={rev} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
               <td className="px-6 py-4">
-                <Link to={`/outputs/${rev}`} className="hover:text-bit-blue">
+                <Link to={`/outputs/${rev}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
                   {rev}
-                  <BsArrowUpRight className="inline text-l ml-1 hover:text-slate-500" />
                 </Link>
               </td>
             </tr>
@@ -74,27 +76,17 @@ export default function SmartContracts(props: { computer: Computer }) {
         </tbody>
       </table>
       {revs.length > 0 && (
-        <nav className="flex items-center justify-between p-4" aria-label="Table navigation">
+        <nav className="flex items-center justify-between" aria-label="Table navigation">
           <ul className="inline-flex items-center -space-x-px">
             <li>
               <button
                 disabled={!isPrevAvailable}
                 onClick={handlePrev}
-                className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white disabled:bg-slate-100 border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+                className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               >
                 <span className="sr-only">Previous</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  ></path>
+                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
                 </svg>
               </button>
             </li>
@@ -102,21 +94,11 @@ export default function SmartContracts(props: { computer: Computer }) {
               <button
                 disabled={!isNextAvailable}
                 onClick={handleNext}
-                className="block px-3 py-2 leading-tight text-gray-500 bg-white disabled:bg-slate-100 border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
               >
                 <span className="sr-only">Next</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  ></path>
+                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
                 </svg>
               </button>
             </li>
