37f67f0ddb74c6aa68913b17c31dee2eff84ee95
Clemens Ley <ley.clemens@gmail.com>
2023-10-06 15:08:51 -0700

    More work on output

--- packages/explorer/src/components/Login.tsx
+++ packages/explorer/src/components/Login.tsx
@@ -45,7 +45,7 @@ function Login(props: {
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button onClick={closeModal} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
               <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
               </svg>
               <span className="sr-only">Close modal</span>
             </button>
@@ -57,7 +57,7 @@ function Login(props: {
                   <input value={password} onChange={(e) => setPassword(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                 </div>
                 <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Don't have a mnemonic? <a href="https://iancoleman.io/bip39/" className="text-blue-700 hover:underline dark:text-blue-500">Generate one here</a>
+                  Don't have a mnemonic? <a href="https://iancoleman.io/bip39/" target="_blank" rel="noreferrer" className="text-blue-700 hover:underline dark:text-blue-500">Generate one here</a>
                 </div>
                 <button onClick={login} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
               </form>
