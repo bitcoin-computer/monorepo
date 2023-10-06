c81f0ed7c95943f58adf2d191b891ab49c53981a
Clemens Ley <ley.clemens@gmail.com>
2023-10-06 12:45:28 -0700

    Make links in Well work

--- packages/explorer/src/components/Well.tsx
+++ packages/explorer/src/components/Well.tsx
@@ -1,19 +1,11 @@
 import { Link } from "react-router-dom"
 import reactStringReplace from 'react-string-replace';
 
-function Well(props: { content: string }) {
-  const { content } = props
+const isRev = /([0-9a-fA-F]{64}:[0-9]+)/g
+const revLink = (rev: string, i: number) => (<Link key={i} to={`/outputs/${rev}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{rev}</Link>)
 
-  // const isRev = /^[0-9a-fA-F]{64}:[0-9]+$/
-  // const revLink = (rev: string) => (<Link to={`/outputs/${rev}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">{rev}</Link>)
-
+export default function Well({ content }: { content: string }) {
   return (<pre className="mt-4 mb-8 p-6 overflow-x-auto leading-normal text-sm rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-blue-4">
-      {content}
-      {/* {reactStringReplace(content, /[0-9a-fA-F]{64}:[0-9]+/, (match, i) => (
-        <span key={i} style={{ color: 'red' }}>{match}</span>
-      ))} */}
+      {reactStringReplace(content, isRev, revLink)}
   </pre>)
 }
-
-export default Well
-
