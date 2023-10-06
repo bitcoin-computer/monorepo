77e43f6d2473706369b91b8d8f9466303faf32d5
Clemens Ley <ley.clemens@gmail.com>
2023-10-06 15:24:59 -0700

    Restructure transaction in explorer

--- packages/explorer/src/components/Transaction.tsx
+++ packages/explorer/src/components/Transaction.tsx
@@ -42,126 +42,125 @@ function Transaction(props: { computer: Computer }) {
     fetch()
   }, [computer, txnData, txn])
 
-  return (
-    <>
-      {
-        <div className="pt-8">
-          <h1 className="mb-2 text-5xl font-extrabold dark:text-white">Transaction</h1>
-          <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">{txn}</p>
+  const transitionComponent = () => (<div>
+      <h2 className="mb-2 text-4xl font-bold dark:text-white">Expression</h2>
+      <Well content={transition.exp} />
 
-          {transition && (
-            <div>
-              <h2 className="mb-2 text-4xl font-bold dark:text-white">Expression</h2>
-              <Well content={transition.exp} />
+      <h2 className="mb-2 text-4xl font-bold dark:text-white">Environment</h2>
+      <Well content={JSON.stringify(transition.env, null, 2)} />
 
-              <h2 className="mb-2 text-4xl font-bold dark:text-white">Environment</h2>
-              <Well content={JSON.stringify(transition.env, null, 2)} />
+      {transition.mod && (<>
+        <h2 className="mb-2 text-4xl font-bold dark:text-white">Module Specifier</h2>
+        <Well content={transition.mod} />
+      </>)}
+    </div>)
 
-              {transition.mod && (<>
-                <h2 className="mb-2 text-4xl font-bold dark:text-white">Module Specifier</h2>
-                <Well content={transition.mod} />
-              </>)}
-            </div>
-          )}
+    const inputsComponent = () => (
+      <div className="relative overflow-x-auto sm:rounded-lg">
+        <h2 className="mb-2 text-4xl font-bold dark:text-white">Inputs</h2>
 
-          {rpcTxnData?.vin && (
-            <div className="relative overflow-x-auto sm:rounded-lg">
-              <h2 className="mb-2 text-4xl font-bold dark:text-white">Inputs</h2>
+        <table className="w-full mt-4 mb-8 text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Transaction Id
+              </th>
+              <th scope="col" className="px-6 py-3 break-keep">
+                Output Number
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Script
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rpcTxnData?.vin?.map((input: any) => {
+              return (
+                <tr key={input.txid} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="px-6 py-4 break-all">
+                    <Link to={`/transactions/${input.txid}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                      {input.txid}
+                    </Link>
+                  </td>
 
-              <table className="w-full mt-4 mb-8 text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Transaction Id
-                    </th>
-                    <th scope="col" className="px-6 py-3 break-keep">
-                      Output Number
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Script
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {rpcTxnData?.vin?.map((input: any) => {
-                    return (
-                      <tr key={input.txid} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td className="px-6 py-4 break-all">
-                          <Link to={`/transactions/${input.txid}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                            {input.txid}
-                          </Link>
-                        </td>
+                  <td className="px-6 py-4">
+                    <Link to={`/outputs/${input.txid}:${input.vout}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                      #{input.vout}
+                    </Link>
+                  </td>
 
-                        <td className="px-6 py-4">
-                          <Link to={`/outputs/${input.txid}:${input.vout}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                            #{input.vout}
-                          </Link>
-                        </td>
+                  <td className="px-6 py-4 break-all">
+                    {input.scriptSig?.asm}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
 
-                        <td className="px-6 py-4 break-all">
-                          {input.scriptSig?.asm}
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          )}
+  const outputsComponent = () => (
+    <div className="relative overflow-x-auto">
+      <h2 className="mb-2 text-4xl font-bold dark:text-white">Outputs</h2>
 
-          {rpcTxnData?.vout && (
-            <div className="relative overflow-x-auto">
-              <h2 className="mb-2 text-4xl font-bold dark:text-white">Outputs</h2>
+      <table className="w-full mt-4 mb-8 text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Number
+            </th>
 
-              <table className="w-full mt-4 mb-8 text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                  <tr>
-                    <th scope="col" className="px-6 py-3">
-                      Number
-                    </th>
+            <th scope="col" className="px-6 py-3">
+              Value
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Type
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Script PubKey
+            </th>
+          </tr>
+        </thead>
+        <tbody>
 
-                    <th scope="col" className="px-6 py-3">
-                      Value
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Type
-                    </th>
-                    <th scope="col" className="px-6 py-3">
-                      Script PubKey
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
+        {rpcTxnData?.vout?.map((output: any) => {
+          return (
+            <tr key={output.n} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="px-6 py-4 break-all">
+                <Link to={`/outputs/${txn}:${output.n}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                  #{output.n}
+                </Link>
+              </td>
 
-                  {rpcTxnData?.vout?.map((output: any) => {
-                    return (
-                      <tr key={output.n} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <td className="px-6 py-4 break-all">
-                          <Link to={`/outputs/${txn}:${output.n}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                            #{output.n}
-                          </Link>
-                        </td>
-
-                        <td className="px-6 py-4">
-                          {output.value}
-                        </td>
-                        <td className="px-6 py-4">
-                          {output.scriptPubKey.type}
-                        </td>
-                        <td className="px-6 py-4 break-all">
-                          {output.scriptPubKey.asm}
-                        </td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </table>
-            </div>
-          )}
-        </div>
-      }
-    </>
+              <td className="px-6 py-4">
+                {output.value}
+              </td>
+              <td className="px-6 py-4">
+                {output.scriptPubKey.type}
+              </td>
+              <td className="px-6 py-4 break-all">
+                {output.scriptPubKey.asm}
+              </td>
+            </tr>)
+          })}
+        </tbody>
+      </table>
+    </div>
   )
+
+  return (<>
+    <div className="pt-8">
+      <h1 className="mb-2 text-5xl font-extrabold dark:text-white">Transaction</h1>
+      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">{txn}</p>
+
+      {transition && transitionComponent()}
+
+      {rpcTxnData?.vin && inputsComponent()}
+
+      {rpcTxnData?.vout && outputsComponent()}
+    </div>
+  </>)
 }
 
 export default Transaction
