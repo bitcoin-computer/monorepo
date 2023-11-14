e774fda048e2923316df01dd6bc307b472d5e4b6
Clemens Ley <ley.clemens@gmail.com>
2023-11-14 19:14:16 -0300

    Cleanup

--- packages/explorer/src/components/Home.tsx
+++ packages/explorer/src/components/Home.tsx
@@ -3,48 +3,7 @@ import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import Loader from "./Loader"
 import Well from "./Well"
-
-type Json = JBasic | JObject | JArray
-type JBasic = undefined | null | boolean | number | string | symbol | bigint
-type JArray = Json[]
-type JObject = { [x: string]: Json }
-
-const isJUndefined = (a: any): a is undefined => typeof a === 'undefined'
-const isJNull = (a: any): a is null => a === null
-const isJBoolean = (a: any): a is boolean => typeof a === 'boolean'
-const isJNumber = (a: any): a is number => typeof a === 'number'
-const isJString = (a: any): a is string => typeof a === 'string'
-const isJSymbol = (a: any): a is symbol => typeof a === 'symbol'
-const isJBigInt = (a: any): a is bigint => typeof a === 'bigint'
-const isJBasic = (a: any): a is JBasic =>
-  isJNull(a) ||
-  isJUndefined(a) ||
-  isJNumber(a) ||
-  isJString(a) ||
-  isJBoolean(a) ||
-  isJSymbol(a) ||
-  isJBigInt(a)
-const isJObject = (a: any): a is JObject => !isJBasic(a) && !Array.isArray(a)
-const isJArray = (a: any): a is JArray => !isJBasic(a) && Array.isArray(a)
-
-const objectEntryMap =
-  (g: (el: [string, Json]) => [string, Json]) =>
-  (object: JObject): JObject =>
-    Object.fromEntries(Object.entries(object).map(g))
-
-const objectMap =
-  (f: (el: Json) => Json) =>
-  (object: JObject): JObject =>
-    objectEntryMap(([key, value]) => [key, f(value)])(object)
-
-const jsonMap =
-  (g: (el: Json) => Json) =>
-  (json: Json): Json => {
-    if (isJBasic(json)) return g(json)
-    if (isJArray(json)) return g(json.map(jsonMap(g)))
-    if (isJObject(json)) return g(objectMap(jsonMap(g))(json))
-    throw new Error('Unsupported type')
-  }
+import { jsonMap, strip } from "../utils"
 
 function ValueComponent({ rev, computer }: {rev: string, computer: Computer}) {
   const [value, setValue] = useState('loading...')
@@ -63,16 +22,7 @@ function ValueComponent({ rev, computer }: {rev: string, computer: Computer}) {
     fetch()
   }, [computer, rev])
 
-  if(errorMsg)
-    return (<Well content={errorMsg} />)
-
-  // @ts-ignore
-  const strip = (value) => {
-    if (isJBasic(value)) return value
-    if (isJArray(value)) return value.map(strip)
-    const { _id, _root, _rev, _amount, _owners, ...rest } = value
-    return rest
-  }
+  if(errorMsg) (<Well content={errorMsg} />)
 
   return (<Well content={JSON.stringify(jsonMap(strip as any)(value as any), null, 2)} />)
 }
