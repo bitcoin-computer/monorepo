4e66b0230f210796ea4fa063f4aa6211256bbc31
Clemens Ley <ley.clemens@gmail.com>
2023-11-14 20:18:42 -0300

    Fix font for name in navbar

--- packages/explorer/src/components/Navbar.tsx
+++ packages/explorer/src/components/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar({ setShowLogin }: {
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to={`/`} className="flex items-center">
           <img src="/logo.png" className="h-8 mr-3 sm:h-9" alt="Bitcoin Computer" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Explorer</span>
+            <span className="self-center text-2xl font-extrabold whitespace-nowrap dark:text-white">TBC Explorer</span>
         </Link>
         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
