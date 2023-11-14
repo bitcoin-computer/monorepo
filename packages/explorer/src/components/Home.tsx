612e686fb709bc1ae64ae0a0093d72dd42229a08
Clemens Ley <ley.clemens@gmail.com>
2023-11-14 20:18:20 -0300

    Use cards on homepage

--- packages/explorer/src/components/Home.tsx
+++ packages/explorer/src/components/Home.tsx
@@ -5,6 +5,14 @@ import Loader from "./Loader"
 import Well from "./Well"
 import { jsonMap, strip } from "../utils"
 
+function Card({ content }: { content: string }) {
+  return (<a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <pre className="font-normal text-gray-700 dark:text-gray-400 text-xs">
+      {content}
+    </pre>
+  </a>)
+}
+
 function ValueComponent({ rev, computer }: {rev: string, computer: Computer}) {
   const [value, setValue] = useState('loading...')
   const [errorMsg, setMsgError] = useState('')
@@ -22,14 +30,75 @@ function ValueComponent({ rev, computer }: {rev: string, computer: Computer}) {
     fetch()
   }, [computer, rev])
 
-  if(errorMsg) (<Well content={errorMsg} />)
+  if(errorMsg) (<Card content={errorMsg} />)
 
-  return (<Well content={JSON.stringify(jsonMap(strip as any)(value as any), null, 2)} />)
+  return (<Card content={JSON.stringify(jsonMap(strip as any)(value as any), null, 2)} />)
+}
+
+function chunk(arr: string[]) {
+  const chunkSize = 4;
+  const chunks = []
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    chunks.push(arr.slice(i, i + chunkSize))
+  }
+  return chunks
+}
+
+function Gallery({ revs, computer }: { revs: string[], computer: any }) {
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 mb-4 mt-4">
+      {chunk(revs).map(
+        (chunk) => (
+          <div className="grid gap-4">
+            {chunk.map((rev: string) => (
+              <div>
+                <Link to={`/outputs/${rev}`} className="font-medium text-blue-600 dark:text-blue-500">
+                  <ValueComponent rev={rev} computer={computer} />
+                </Link>
+              </div>))
+            }
+          </div>
+        ))}
+      </div>
+    )
+}
+
+function Pagination({ isPrevAvailable, handlePrev, isNextAvailable, handleNext }: any) {
+  return (
+    <nav className="flex items-center justify-between" aria-label="Table navigation">
+      <ul className="inline-flex items-center -space-x-px">
+        <li>
+          <button
+            disabled={!isPrevAvailable}
+            onClick={handlePrev}
+            className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="sr-only">Previous</span>
+            <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
+            </svg>
+          </button>
+        </li>
+        <li>
+          <button
+            disabled={!isNextAvailable}
+            onClick={handleNext}
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            <span className="sr-only">Next</span>
+            <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
+            </svg>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default function Home(props: { computer: Computer }) {
   const { computer } = props
-  const contractsPerPage = 10
+  const contractsPerPage = 12
 
   const [isLoading, setIsLoading] = useState(false)
   const [pageNum, setPageNum] = useState(0)
@@ -76,67 +145,11 @@ export default function Home(props: { computer: Computer }) {
 
   return (
     <div className="relative sm:rounded-lg pt-4">
-      <h1 className="mb-4 text-5xl font-extrabold dark:text-white">Bitcoin Computer Explorer</h1>
-
       <h2 className="mb-2 text-4xl font-bold dark:text-white">Smart Objects</h2>
 
-      <table className="w-full mt-4 mb-4 text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="max-w-[50%] px-6 py-3">
-              Value
-            </th>
-            <th scope="col" className="max-w-[50%] px-6 py-3">
-              Details
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {revs.map((rev) => (
-            <tr key={rev} className="max-w-[50%] overflow-x-scroll bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <td className="px-6 py-4 overflow-x-scroll">
-                <ValueComponent rev={rev} computer={computer} />
-              </td>
+      <Gallery revs={revs} computer={computer} />
 
-              <td className="max-w-[50%] px-6 py-4">
-                <Link to={`/outputs/${rev}`} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                  {rev}
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {revs.length > 0 && (
-        <nav className="flex items-center justify-between" aria-label="Table navigation">
-          <ul className="inline-flex items-center -space-x-px">
-            <li>
-              <button
-                disabled={!isPrevAvailable}
-                onClick={handlePrev}
-                className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <span className="sr-only">Previous</span>
-                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-                </svg>
-              </button>
-            </li>
-            <li>
-              <button
-                disabled={!isNextAvailable}
-                onClick={handleNext}
-                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                <span className="sr-only">Next</span>
-                <svg className="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                </svg>
-              </button>
-            </li>
-          </ul>
-        </nav>
-      )}
+      <Pagination revs={revs} isPrevAvailable={isPrevAvailable} handlePrev={handlePrev} isNextAvailable={isNextAvailable} handleNext={handleNext} />
       {isLoading && <Loader />}
     </div>
   )
