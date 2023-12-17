385e591bf2ba1fde487a9add6ac95acda901cab1
Clemens Ley <ley.clemens@gmail.com>
2023-12-17 19:50:24 -0300

    Refactor wallet

--- packages/explorer/src/components/Wallet.tsx
+++ packages/explorer/src/components/Wallet.tsx
@@ -1,19 +1,99 @@
-import { Computer } from "@bitcoin-computer/lib"
+import { initFlowbite } from "flowbite"
 import { useCallback, useEffect, useState } from "react"
 import { HiRefresh } from "react-icons/hi"
-import { chunk } from "../utils"
 
-function Card({ content }: { content: string }) {
-  return (<div className="mt-2 mb-2 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <pre className="font-normal text-gray-700 dark:text-gray-400 text-xs">
-      {content}
-    </pre>
+const Balance = ({ refreshBalance, balance }: any) => (<div className="mb-6">
+  <span className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+    Balance
+    <HiRefresh
+      onClick={refreshBalance}
+      className="w-4 h-4 ml-1 inline cursor-pointer text-gray-500 dark:text-gray-400 hover:text-slate-700 dark:hover:text-slate-100"
+    ></HiRefresh>
+  </span>
+  <p className="mb-4 font-mono text-xs text-gray-500 dark:text-gray-400">
+    {balance / 1e8} LTC
+  </p>
+  </div>)
+
+const Address = ({ computer }: any) => (<div className="mb-6">
+    <span className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+      Address
+    </span>
+    <p className="mb-4 font-mono text-xs text-gray-500 dark:text-gray-400">
+      {computer.getAddress()}
+    </p>
+  </div>)
+
+const PublicKey = ({ computer }: any) => (<div className="mb-6">
+    <span className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+      Public Key
+    </span>
+    <p className="mb-4 font-mono text-xs text-gray-500 dark:text-gray-400 break-words">
+      {computer.getPublicKey()}
+    </p>
+  </div>)
+
+const Mnemonic = ({ computer, showMnemonic, setShowMnemonic }: any) => {
+  const Heading = () => (<span className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+    Mnemonic
+  </span>)
+
+  if (showMnemonic) return (<div className="mb-6">
+    <Heading />
+    <p className="mb-1 font-mono text-xs text-gray-500 dark:text-gray-400 break-words">
+      {computer.getMnemonic()}
+    </p>
+
+    <button
+      onClick={() => setShowMnemonic(false)}
+      className="text-sm text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 underline"
+    >
+      Hide
+    </button>
+  </div>)
+  else return (<div className="mb-6">
+    <Heading />
+    <button
+      onClick={() => setShowMnemonic(true)}
+      className="text-sm text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500 underline"
+    >
+      Show
+    </button>
+    <br />
   </div>)
 }
 
-export default function Wallet2({ computer }: { computer: Computer }) {
-  const [balance, setBalance] = useState(0)
+const LogOut = ({ logout }: any) => (<>
+  <div className="mb-6">
+    <span className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+      Log out
+    </span>
+    <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+      Logging out will delete your mnemonic, make sure to write it down before logging out.
+    </p>
+  </div><div className="grid grid-cols-2 gap-4">
+    <button
+      onClick={logout}
+      className="rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+    >
+      Log out
+    </button>
+  </div>
+</>)
+
+export default function Wallet({ id, computer }: any) {
+  const [balance, setBalance] = useState<number>(0)
   const [showMnemonic, setShowMnemonic] = useState(false)
+
+  const logout = () => {
+    localStorage.removeItem("BIP_39_KEY")
+    localStorage.removeItem("CHAIN")
+    window.location.href = "/"
+  }
+
+  useEffect(() => {
+    initFlowbite()
+  }, [])
 
   const refreshBalance = useCallback(async () => {
     try {
@@ -23,72 +103,53 @@ export default function Wallet2({ computer }: { computer: Computer }) {
     }
   }, [computer])
 
-  useEffect(() => {
-    ;(async () => {
-      await refreshBalance()
-    })()
-  }, [refreshBalance])
+  return (
+    <>
+      <span className="bg-gray-900/50 dark:bg-gray-900/80 sr-only"></span>
+      <div
+        id={`drawer-${id}`}
+        className="fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform translate-x-full bg-white w-96 dark:bg-gray-800"
+        tabIndex={-1}
+        aria-labelledby={`drawer-${id}-label`}
+        aria-hidden="true"
+      >
+        <h5
+          id={`drawer-label-${id}`}
+          className="mb-8 text-lg font-bold dark:text-white"
+        >
+          Wallet
+        </h5>
 
-  const logout = () => {
-    localStorage.removeItem("BIP_39_KEY")
-    localStorage.removeItem("CHAIN")
-    window.location.href = "/"
-  }
+        <button
+          type="button"
+          data-drawer-hide={`drawer-${id}`}
+          aria-controls={`drawer-${id}`}
+          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <svg
+            className="w-3 h-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+          <span className="sr-only">Close menu</span>
+        </button>
 
-  const mnemonicWell = () => {
-    const mnemonicChunks = chunk(computer.getMnemonic().split(' '))
-    const mnemonicChunksString = (mnemonicChunks.map((chunk) => chunk.join(' ') + '\n')).join(' ')
-    return (<>
-      <Card content={' ' + mnemonicChunksString} />
-      <button onClick={() => setShowMnemonic(false)} className="mb-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
-        Hide Mnemonic
-      </button>
-    </>)
-    }
-
-  const showMnemonicLink = () => (<>
-      <button onClick={() => setShowMnemonic(true)} className="mb-4 font-medium text-blue-600 dark:text-blue-500 hover:underline">
-        Show Mnemonic
-      </button>
-      <br />
-    </>)
-
-  return (<>
-    <input type="checkbox" data-menu id="wallet-opener" hidden />
-    <aside className="WalletDrawer" role="menu" id="menu" aria-labelledby="openmenu">
-      <nav className="Menu bg-white w-80 dark:bg-gray-800 text-left">
-          {/* 
-          <button type="button" data-drawer-hide="drawer-right-example" aria-controls="drawer-right-example" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 right-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
-            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-            </svg>
-            <span className="sr-only">Close menu</span>
-          </button>
-          */}
-          <div className="mt-8 p-4 text-sm text-gray-900 dark:text-gray-400 break-words">
-            <h2 className="text-4xl mb-2 font-bold dark:text-white">Wallet</h2>
-            <h6 className="text-lg font-bold dark:text-white">Balance</h6>
-            <HiRefresh onClick={refreshBalance} className="ml-2 inline hover:text-slate-500 cursor-pointer"></HiRefresh>
-            {balance / 1e8} LTC
-
-            <h6 className="mt-2 text-lg font-bold dark:text-white">Address</h6>
-            {computer.getAddress()}
-
-            <h6 className="mt-2 text-lg font-bold dark:text-white break-all">Public Key</h6>
-            {computer.getPublicKey()}
-
-            <h6 className="mt-2 text-lg font-bold dark:text-white break-all">Mnemonic</h6>
-            {showMnemonic ? (mnemonicWell()) : (showMnemonicLink())}
-
-            <h6 className="text-lg font-bold dark:text-white">Log out</h6>
-            <p className="mb-2">Logging out will delete your mnemonic, make sure to write it down before logging out.</p>
-
-            <button onClick={logout} type="button" className="mt-2 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-              Log Out
-            </button>
-          </div>
-      </nav>
-      <label htmlFor="wallet-opener" className="MenuOverlay bg-gray-900 bg-opacity-50 dark:bg-opacity-80"></label>
-    </aside>
-  </>)
+        <Balance refreshBalance={refreshBalance} balance={balance} />
+        <Address computer={computer} />
+        <PublicKey computer={computer} />
+        <Mnemonic computer={computer} showMnemonic={showMnemonic} setShowMnemonic={setShowMnemonic} />
+        <LogOut logout={logout} />
+      </div>
+    </>
+  )
 }
