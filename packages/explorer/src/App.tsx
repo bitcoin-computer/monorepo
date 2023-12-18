73d1a859c75ba3b4346169cb8922983d9669cba7
Clemens Ley <ley.clemens@gmail.com>
2023-12-18 19:28:34 -0300

    REfactoring

--- packages/explorer/src/App.tsx
+++ packages/explorer/src/App.tsx
@@ -16,35 +16,27 @@ import Playground from "./components/playground/Playground"
 function App() {
   const mnemonic = localStorage.getItem("BIP_39_KEY") || ""
   const chain = localStorage.getItem("CHAIN") || ""
-  const url = (network: string) =>
-    network === "testnet" ? "https://node.bitcoincomputer.io" : "http://127.0.0.1:1031"
+  const url = (network: string) => network === "testnet" ? "https://node.bitcoincomputer.io" : "http://127.0.0.1:1031"
   const getConf = (network: string) => ({ chain, network, mnemonic, url: url(network) })
   const config = getConf("regtest")
   const [computer, setComputer] = useState(new Computer(config))
 
   return (
     <BrowserRouter>
+      <Wallet id={"wallet"} computer={computer} />
+      <Login config={config} setComputer={setComputer} />
       <NavBar computer={computer} />
       <div className="p-8 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-        {/* <Drawer /> */}
-        <Wallet id={"wallet"} computer={computer} />
         <Routes>
           <Route path="/" element={<Home computer={computer}></Home>} />
           <Route path="/blocks" element={<Blocks computer={computer}></Blocks>} />
           <Route path="/playground" element={<Playground computer={computer} />} />
-          <Route
-            path="/transactions/:txn"
-            element={<Transaction computer={computer}></Transaction>}
-          />
+          <Route path="/transactions/:txn" element={<Transaction computer={computer}></Transaction>} />
           <Route path="/blocks/:block" element={<Block computer={computer}></Block>} />
           <Route path="/objects/:rev" element={<SmartObject computer={computer}></SmartObject>} />
           <Route path="/modules/:rev" element={<Module computer={computer}></Module>} />
           <Route path="*" element={<Navigate to="/" replace={true} />} />
         </Routes>
-        <Login
-          config={config}
-          setComputer={setComputer}
-        />
       </div>
     </BrowserRouter>
   )
--- packages/explorer/src/components/Login.tsx
+++ packages/explorer/src/components/Login.tsx
@@ -36,21 +36,22 @@ function Login({ config, setComputer }: {
     modal.hide()
   }
 
-  const title = "Sign in"
-  const body = () => (<form className="space-y-6" action="#">
-    <div>
-      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sign in with a BIP 39 mnemonic code</label>
-      <input value={password} onChange={(e) => setPassword(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
-    </div>
-    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-      Don't have a mnemonic? <a href="https://iancoleman.io/bip39/" target="_blank" rel="noreferrer" className="text-blue-700 hover:underline dark:text-blue-500">Generate one here</a>
-    </div>
-  </form>)
+  const body = () =>
+    <form className="space-y-6" action="#">
+      <div>
+        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sign in with a BIP 39 mnemonic code</label>
+        <input value={password} onChange={(e) => setPassword(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+      </div>
+      <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Don't have a mnemonic? <a href="https://iancoleman.io/bip39/" target="_blank" rel="noreferrer" className="text-blue-700 hover:underline dark:text-blue-500">Generate one here</a>
+      </div>
+    </form>
 
-  const footer = () => <button onClick={login} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
+  const footer = () =>
+    <button onClick={login} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
 
   return <>
-    <Modal title={title} body={body} footer={footer} id="sign-in-modal"/>
+    <Modal title="Sign in" body={body} footer={footer} id="sign-in-modal"/>
     {show && <SnackBar message={message} success={success} setShow={setShow} />}
   </>
 }
