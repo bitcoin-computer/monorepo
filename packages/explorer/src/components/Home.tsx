455eb37de5f5e82eda4688c981dbd7db0a23e820
Clemens Ley <ley.clemens@gmail.com>
2023-12-28 17:05:53 -0300

    Make homepage more robust to errors

--- packages/explorer/src/components/Home.tsx
+++ packages/explorer/src/components/Home.tsx
@@ -6,35 +6,45 @@ import { chunk, jsonMap, strip, toObject } from "../utils"
 import { getComputer } from "@bitcoin-computer/components"
 import { initFlowbite } from "flowbite"
 
-function HomePageCard({ content }: { content: string }) {
+function HomePageCard({ content }: any) {
   return (
     <div className="block w-80 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <pre className="font-normal overflow-auto text-gray-700 dark:text-gray-400 text-xs">
-        {content}
+        {content()}
       </pre>
     </div>
   )
 }
 
 function ValueComponent({ rev, computer }: { rev: string; computer: Computer }) {
-  const [value, setValue] = useState("loading...")
+  const [value, setValue] = useState<any>("loading...")
   const [errorMsg, setMsgError] = useState("")
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
     const fetch = async () => {
       try {
         const synced = await computer.sync(rev)
-        setValue(synced as any)
+        setValue(toObject(jsonMap(strip)(synced)))
       } catch (err) {
         if (err instanceof Error) setMsgError(`Error: ${err.message}`)
       }
+      setLoading(false)
     }
     fetch()
   }, [computer, rev])
 
-  if (errorMsg) <HomePageCard content={errorMsg} />
+  const loadingContent = () => <>
+    <svg aria-hidden="true" role="status" className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+      <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2"/>
+    </svg>
+    Loading...
+  </>
 
-  return <HomePageCard content={toObject(jsonMap(strip as any)(value as any))} />
+  return loading
+    ? <HomePageCard content={loadingContent} />
+    : <HomePageCard content={() => errorMsg || value} />
 }
 
 function Gallery({ revs, computer }: { revs: string[]; computer: any }) {
@@ -42,10 +52,7 @@ function Gallery({ revs, computer }: { revs: string[]; computer: any }) {
     <div className="flex flex-wrap gap-4 mb-4 mt-4">
       {revs.map((rev, i) => (
         <div key={i}>
-          <Link
-            to={`/objects/${rev}`}
-            className="block font-medium text-blue-600 dark:text-blue-500"
-          >
+          <Link to={`/objects/${rev}`} className="block font-medium text-blue-600 dark:text-blue-500">
             <ValueComponent rev={rev} computer={computer} />
           </Link>
         </div>
@@ -114,7 +121,7 @@ function Pagination({ isPrevAvailable, handlePrev, isNextAvailable, handleNext }
 export default function Home() {
   const contractsPerPage = 12
   const [computer] = useState(getComputer())
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [pageNum, setPageNum] = useState(0)
   const [isNextAvailable, setIsNextAvailable] = useState(true)
   const [isPrevAvailable, setIsPrevAvailable] = useState(pageNum > 0)
@@ -129,15 +136,10 @@ export default function Home() {
   useEffect(() => {
     const fetch = async () => {
       try {
-        setIsLoading(true)
-        const queryParms: any = {
-          offset: contractsPerPage * pageNum,
-          limit: contractsPerPage + 1,
-        }
-        if (publicKey) {
-          queryParms["publicKey"] = publicKey
-        }
-
+        const offset = contractsPerPage * pageNum
+        const limit = contractsPerPage + 1
+        const queryParms: Record<string, string | number> = { offset, limit }
+        if (publicKey) queryParms["publicKey"] = publicKey
         const queryRevs = await computer.query(queryParms)
 
         if (queryRevs.length <= contractsPerPage) {
@@ -147,11 +149,10 @@ export default function Home() {
           setIsNextAvailable(true)
         }
         setRevs(queryRevs)
-        setIsLoading(false)
       } catch (error) {
-        setIsLoading(false)
         console.log("Error loading revisions", error)
       }
+      setIsLoading(false)
     }
     fetch()
   }, [computer, revs.length, pageNum, publicKey])
@@ -163,9 +164,7 @@ export default function Home() {
 
   const handlePrev = async () => {
     setIsNextAvailable(true)
-    if (pageNum - 1 === 0) {
-      setIsPrevAvailable(false)
-    }
+    if (pageNum - 1 === 0) setIsPrevAvailable(false)
     setPageNum(pageNum - 1)
   }
 
