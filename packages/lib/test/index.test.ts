4fa31ab12fb3f0b77a99f0c143e2e4a86c436954
Clemens Ley <ley.clemens@gmail.com>
2024-03-01 20:04:55 -0300

    Test that buyer cannot underpay in 404 contract

--- packages/lib/test/index.test.ts
+++ packages/lib/test/index.test.ts
@@ -1,8 +1,8 @@
 import chai, { expect } from 'chai'
+import chaiMatchPattern from 'chai-match-pattern'
 // @ts-ignore
 import { Computer, Contract } from '@bitcoin-computer/lib'
-import chaiMatchPattern from 'chai-match-pattern'
-import { Transaction as BTransaction } from '@bitcoin-computer/nakamotojs'
+import { Transaction } from '@bitcoin-computer/nakamotojs'
 
 chai.use(chaiMatchPattern)
 const _ = chaiMatchPattern.getLodashModule()
@@ -11,12 +11,6 @@ const randomPublicKey = '023e21361b53bb2e625cc1f41d18b35ae882e88d8d107df1c3711fa
 const randomRev = '0000000000000000000000000000000000000000000000000000000000000000:0'
 const mockedRev = `mock:${randomRev}`
 const symbol = ''
-
-const isLocation = (string: string): boolean => {
-  const [txId, num] = string.split(':')
-  const float = parseFloat(num)
-  return [64, 65].includes(txId.length) && !Number.isNaN(float) && Number.isFinite(float)
-}
 
 const RLTC: {
   network: 'regtest',
@@ -110,12 +104,12 @@ class PaymentMock {
   _amount: number
   _owners: string[]
 
-  constructor() {
+  constructor(amount: number) {
     this._id = mockedRev
     this._rev = mockedRev
     this._root = mockedRev
     this._owners = [randomPublicKey]
-    this._amount = 7860
+    this._amount = amount
   }
 
   transfer(to: string) {
@@ -127,10 +121,11 @@ class Payment extends Contract {
   _id: string
   _rev: string
   _root: string
+  _amount: number
   _owners: string[]
 
-  constructor(owner: string) {
-    super({ _owners: [owner] })
+  constructor(owner: string, _amount: number) {
+    super({ _owners: [owner], _amount })
   }
 
   transfer(to: string) {
@@ -439,7 +434,10 @@ describe('Swap', () => {
 
 describe('Sell', () => {
   let tx: any
+  let txClone: any
   let sellerPublicKey: string
+  const nftPrice = 0.1e8
+  const fee = 100000
   
   describe('Creating an NFT and an offer to sell', () => {
     let nft: NFT
@@ -457,8 +455,8 @@ describe('Sell', () => {
     })
 
     it('Seller creates a swap transaction for the NFT with the desired price', async () => {
-      const mock = new PaymentMock()
-      const { SIGHASH_SINGLE, SIGHASH_ANYONECANPAY } = BTransaction
+      const mock = new PaymentMock(nftPrice)
+      const { SIGHASH_SINGLE, SIGHASH_ANYONECANPAY } = Transaction
 
       ;({ tx } = await seller.encode({
         exp: `${Swap} Swap.exec(nft, payment)`,
@@ -468,12 +466,71 @@ describe('Sell', () => {
         inputIndex: 0,
         fund: false,
       }))
+
+      txClone = tx.clone()
     })
 
     it('The first inputs has been signed by seller, the second input is unsigned', () => {
       expect(tx.ins).to.have.lengthOf(2)
       expect(tx.ins[0].script).to.have.lengthOf.above(0)
       expect(tx.ins[1].script).to.have.lengthOf(0)
+    })
+
+    it("The first output's value is nftPrice, the second is min-non-dust amount", () => {
+      expect(tx.outs[0].value).eq(nftPrice)
+      expect(tx.outs[1].value).eq(7860)
+    })
+  })
+
+  describe('Failing to steal the nft', () => {
+    const thief = new Computer(RLTC)
+    let tooLowPayment: Payment
+    let txId: string
+
+    before("Fund Thief's wallet", async () => {
+      await thief.faucet(nftPrice + fee)
+    })
+
+    it('Thief creates a payment object with half the asking price', async () => {
+      tooLowPayment = await thief.new(Payment, [thief.getPublicKey(), nftPrice / 2])
+
+      // @ts-ignore
+      expect(tooLowPayment).matchPattern({
+        _id: _.isString,
+        _rev: _.isString,
+        _root: _.isString,
+        _owners: [thief.getPublicKey()],
+        _amount: nftPrice / 2,
+      })
+    })
+
+    it("Thief update's the swap transaction maliciously to receive the NFT at half the price", () => {
+      const [paymentTxId, paymentIndex] = tooLowPayment._rev.split(':')
+      txClone.updateInput(1, { txId: paymentTxId, index: parseInt(paymentIndex, 10) })
+      // @ts-ignore
+      txClone.updateOutput(1, { scriptPubKey: thief.toScriptPubKey() })
+
+      // this is where the thief tries to alter the transaction in order
+      // to buy the nft at half the price
+      txClone.updateOutput(0, { value: tooLowPayment._amount })
+    })
+
+    it('Thief funds the swap transaction', async () => {
+      await thief.fund(txClone)
+    })
+
+    it('Thief signs the swap transaction', async () => {
+      await thief.sign(txClone)
+    })
+
+    it('When Thief broadcast the swap transaction an error is thrown', async () => {
+      try {
+        txId = await thief.broadcast(txClone)
+        expect(true).eq(false)
+      } catch(err) {
+        if(err instanceof Error)
+          expect(err.message).eq('mandatory-script-verify-flag-failed (Signature must be zero for failed CHECK(MULTI)SIG operation)')
+      }
     })
   })
 
@@ -484,11 +541,12 @@ describe('Sell', () => {
     let txId: string
 
     before("Fund Buyers's wallet", async () => {
-      await buyer.faucet(0.1e8)
+      await buyer.faucet(nftPrice + fee)
+
     })
 
     it('Buyer creates a payment object', async () => {
-      payment = await buyer.new(Payment, [buyer.getPublicKey()])
+      payment = await buyer.new(Payment, [buyer.getPublicKey(), nftPrice])
 
       // @ts-ignore
       expect(payment).matchPattern({
@@ -496,7 +554,7 @@ describe('Sell', () => {
         _rev: _.isString,
         _root: _.isString,
         _owners: [buyer.getPublicKey()],
-        _amount: 7860,
+        _amount: nftPrice,
       })
     })
 
@@ -525,7 +583,7 @@ describe('Sell', () => {
       expect(env.payment._owners).deep.eq([sellerPublicKey])
     })
 
-    it('Seller should get the payment and buyer should get the nft', async () => {
+    it('Buyer now owns the nft', async () => {
       const { env } = await computer.sync(txId) as any
       expect(env.nft._owners).deep.eq([buyer.getPublicKey()])
     })
