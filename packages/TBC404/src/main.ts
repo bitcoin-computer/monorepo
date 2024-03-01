2115524de913981ae35e7c5ff649f97b3e9c5aa1
Clemens Ley <ley.clemens@gmail.com>
2024-03-01 20:14:19 -0300

    Clone package TBC404 from nodejs-template

---
+++ packages/TBC404/src/main.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+import { Contract } from '@bitcoin-computer/lib'
+
+class Counter extends Contract {
+  n: number
+
+  constructor() {
+    super({ n: 0 })
+  }
+
+  inc() {
+    this.n += 1
+  }
+}
+
+export { Counter }
