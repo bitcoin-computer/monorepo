67bb5476aef2df55fb461a9f41fd3dd45869aabd
Clemens Ley <ley.clemens@gmail.com>
2024-03-21 16:05:45 -0700

    TBC20: start refactoring test file to make it more like nft.test.ts

--- packages/TBC20/test/token.test.ts
+++ packages/TBC20/test/token.test.ts
@@ -14,13 +14,8 @@ dotenv.config({ path: '../../.env'})
 
 const url = process.env.BCN_URL
 
-const computer = new Computer({ url })
-const computer2 = new Computer({ url })
-
-const RLTC = {
-  url: 'http://127.0.0.1:1031',
-  network: 'regtest' as any,
-}
+const sender = new Computer({ url })
+const receiver = new Computer({ url })
 
 function sleep(delay: number): Promise<void> {
   return new Promise((resolve) => {
@@ -28,181 +23,168 @@ function sleep(delay: number): Promise<void> {
   })
 }
 
-
 before(async () => {
-  await computer.faucet(1e7)
-  await computer2.faucet(1e7)
+  await sender.faucet(1e7)
 })
 
 describe('Token', () => {
-  describe('Constructor', () => {
-    it('should create a Javascript object', () => {
-      expect(Token).not.to.be.undefined
-      expect(typeof Token).to.eq('function')
+  describe('Using fungible tokens without a helper class', () => {
+    let token: Token
 
-      const token = new Token('to', 3, 'test')
-      expect(token).not.to.be.undefined
+    describe('Minting a fungible token', async () => {
+      it('Sender mints a token', async () => {
+        token = await sender.new(Token, [sender.getPublicKey(), 3, 'test'])
+      })
+
+      it('The meta data should be set', async () => {
+        expect(token.tokens).to.eq(3)
+        expect(token._owners).deep.equal([sender.getPublicKey()])
+        expect(token.name).to.eq('test')
+        expect(token.symbol).to.eq('')
+        expect(token._id).to.be.a('string')
+        expect(token._rev).to.be.a('string')
+        expect(token._root).to.be.a('string')
+      })
     })
 
-    it('should create a smart object', async () => {
-      const publicKeyString = computer.getPublicKey()
+    describe('Transferring the NFT', () => {
+      let newToken: Token
 
-      const token = await computer.new(Token, [publicKeyString, 3, 'test'])
-      expect(token.tokens).to.eq(3)
-      expect(token._owners).deep.equal([publicKeyString])
-      expect(token.name).to.eq('test')
-      expect(token.symbol).to.eq('')
-      expect(token._id).to.be.a('string')
-      expect(token._rev).to.be.a('string')
-      expect(token._root).to.be.a('string')
-    })
-  })
+      it('Sender transfers the NFT to receiver', async () => {      
+        newToken = await token.transfer(receiver.getPublicKey(), 1)
+      })
 
-  describe('transfer', () => {
-    it('Should update a smart object', async () => {
-      const publicKeyString = computer.getPublicKey()
-      const publicKeyString2 = computer2.getPublicKey()
+      it('The meta data of token should be set correctly', () => {
+        expect(token.tokens).to.eq(2)
+        expect(token._owners).deep.equal([sender.getPublicKey()])
+        expect(token.name).to.eq('test')
+        expect(token.symbol).to.eq('')
+        expect(token._id).to.be.a('string')
+        expect(token._rev).to.be.a('string')
+        expect(token._root).to.be.a('string')
+      })
 
-      const token = await computer.new(Token, [publicKeyString, 3, 'test'])
-      const newToken = await token.transfer(publicKeyString2, 1)
-      expect(token.tokens).to.eq(2)
-      expect(token._owners).deep.equal([publicKeyString])
-      expect(token.name).to.eq('test')
-      expect(token.symbol).to.eq('')
-      expect(token._id).to.be.a('string')
-      expect(token._rev).to.be.a('string')
-      expect(token._root).to.be.a('string')
-
-      expect(newToken.tokens).to.eq(1)
-      expect(newToken._owners).deep.equal([publicKeyString2])
-      expect(newToken.name).to.eq('test')
-      expect(newToken.symbol).to.eq('')
-      expect(newToken._id).to.be.a('string')
-      expect(newToken._rev).to.be.a('string')
-      expect(newToken._root).to.be.a('string')
-    })
-  })
-})
-
-describe('TBC20', () => {
-  describe('Constructor', () => {
-    it('Should create a new TBC20 object', async () => {
-      const tbc20 = new TBC20('test', 'TST', computer)
-      expect(tbc20).not.to.be.undefined
-      expect(tbc20.computer).to.be.a('object')
-      expect(tbc20.mintId).to.be.undefined
-      expect(tbc20.name).to.eq('test')
-      expect(tbc20.symbol).to.eq('TST')
+      it('The meta data of newToken should be set correctly', () => {
+        expect(newToken.tokens).to.eq(1)
+        expect(newToken._owners).deep.equal([receiver.getPublicKey()])
+        expect(newToken.name).to.eq('test')
+        expect(newToken.symbol).to.eq('')
+        expect(newToken._id).to.be.a('string')
+        expect(newToken._rev).to.be.a('string')
+        expect(newToken._root).to.be.a('string')
+      })
     })
   })
 
-  describe('mint', () => {
-    const tbc20 = new TBC20('test', 'TST', computer)
-    let mintId: string
-
-    it('Should create the tbc20 object', async () => {
-      const publicKey = tbc20.computer.getPublicKey()
-      mintId = await tbc20.mint(publicKey, 200)
-      expect(mintId).not.to.be.undefined
-      expect(typeof mintId).to.eq('string')
-      expect(mintId.length).to.be.greaterThan(64)
+  describe('Using fungible tokens with a helper class', () => {
+    describe('mint', () => {
+      const tbc20 = new TBC20('test', 'TST', sender)
+      let mintId: string
+  
+      it('Should create the tbc20 object', async () => {
+        const publicKey = tbc20.computer.getPublicKey()
+        mintId = await tbc20.mint(publicKey, 200)
+        expect(mintId).not.to.be.undefined
+        expect(typeof mintId).to.eq('string')
+        expect(mintId.length).to.be.greaterThan(64)
+      })
+  
+      it('Should mint a root token', async () => {
+        const rootToken: any = await sender.sync(mintId)
+        expect(rootToken).not.to.be.undefined
+        expect(rootToken._id).to.eq(mintId)
+        expect(rootToken._rev).to.eq(mintId)
+        expect(rootToken._root).to.eq(mintId)
+        expect(rootToken.tokens).to.eq(200)
+        expect(rootToken.name).to.eq('test')
+        expect(rootToken.symbol).to.eq('TST')
+      })
     })
-
-    it('Should mint a root token', async () => {
-      const rootToken: any = await computer.sync(mintId)
-      expect(rootToken).not.to.be.undefined
-      expect(rootToken._id).to.eq(mintId)
-      expect(rootToken._rev).to.eq(mintId)
-      expect(rootToken._root).to.eq(mintId)
-      expect(rootToken.tokens).to.eq(200)
-      expect(rootToken.name).to.eq('test')
-      expect(rootToken.symbol).to.eq('TST')
+  
+    describe('totalSupply', () => {
+      it('Should return the supply of tokens', async () => {
+        const tbc20 = new TBC20('test', 'TST', sender)
+        const publicKey = tbc20.computer.getPublicKey()
+        await tbc20.mint(publicKey, 200)
+        const supply = await tbc20.totalSupply()
+        expect(supply).to.eq(200)
+      })
     })
-  })
-
-  describe('totalSupply', () => {
-    it('Should return the supply of tokens', async () => {
-      const tbc20 = new TBC20('test', 'TST', computer)
-      const publicKey = tbc20.computer.getPublicKey()
-      await tbc20.mint(publicKey, 200)
-      const supply = await tbc20.totalSupply()
-      expect(supply).to.eq(200)
+  
+    describe('balanceOf', () => {
+      it('Should throw an error if the mint id is not set', async () => {
+        const publicKeyString = sender.getPublicKey()
+  
+        const tbc20 = new TBC20('test', 'TST', sender)
+        expect(tbc20).not.to.be.undefined
+        try {
+          await tbc20.balanceOf(publicKeyString)
+          expect(true).to.eq('false')
+        } catch (err) {
+          expect(err.message).to.eq('Please set a mint id.')
+        }
+      })
+  
+      it('Should compute the balance', async () => {
+        const tbc20 = new TBC20('test', 'TST', sender)
+        const publicKey = tbc20.computer.getPublicKey()
+        await tbc20.mint(publicKey, 200)
+        await sleep(200)
+        const res = await tbc20.balanceOf(publicKey)
+        expect(res).to.eq(200)
+      })
     })
-  })
-
-  describe('balanceOf', () => {
-    it('Should throw an error if the mint id is not set', async () => {
-      const publicKeyString = computer.getPublicKey()
-
-      const tbc20 = new TBC20('test', 'TST', computer)
-      expect(tbc20).not.to.be.undefined
-      try {
-        await tbc20.balanceOf(publicKeyString)
-        expect(true).to.eq('false')
-      } catch (err) {
-        expect(err.message).to.eq('Please set a mint id.')
-      }
-    })
-
-    it('Should compute the balance', async () => {
-      const tbc20 = new TBC20('test', 'TST', computer)
-      const publicKey = tbc20.computer.getPublicKey()
-      await tbc20.mint(publicKey, 200)
-      await sleep(200)
-      const res = await tbc20.balanceOf(publicKey)
-      expect(res).to.eq(200)
-    })
-  })
-
-  describe('transfer', () => {
-    it('Should transfer a token', async () => {
-      const computer2 = new Computer()
-      const tbc20 = new TBC20('test', 'TST', computer)
-      const publicKey = tbc20.computer.getPublicKey()
-      await tbc20.mint(publicKey, 200)
-      await sleep(200)
-      await tbc20.transfer(computer2.getPublicKey(), 20)
-      await sleep(200)
-      const res = await tbc20.balanceOf(publicKey)
-      expect(res).to.eq(180)
-    })
-
-    it('Should transfer random amounts to different people', async () => {
-      const computer2 = new Computer()
-      const computer3 = new Computer()
-      const tbc20 = new TBC20('multiple', 'MULT', computer)
-      const publicKey = tbc20.computer.getPublicKey()
-      await tbc20.mint(publicKey, 200)
-
-      const amount2 = Math.floor(Math.random() * 100)
-      const amount3 = Math.floor(Math.random() * 100)
-      await sleep(200)
-      await tbc20.transfer(computer2.getPublicKey(), amount2)
-      await sleep(200)
-      await tbc20.transfer(computer3.getPublicKey(), amount3)
-      await sleep(200)
-      const res = await tbc20.balanceOf(publicKey)
-      expect(res).to.eq(200 - amount2 - amount3)
-
-      const res2 = await tbc20.balanceOf(computer2.getPublicKey())
-      expect(res2).to.eq(amount2)
-
-      const res3 = await tbc20.balanceOf(computer3.getPublicKey())
-      expect(res3).to.eq(amount3)
-    })
-
-    it('Should fail if the amount is greater than the balance', async () => {
-      const computer2 = new Computer()
-      const tbc20 = new TBC20('test', 'TST', computer)
-      const publicKey = tbc20.computer.getPublicKey()
-      await tbc20.mint(publicKey, 200)
-      await sleep(200)
-      try {
-        await tbc20.transfer(computer2.getPublicKey(), 201)
-        expect(true).to.eq('false')
-      } catch (err) {
-        expect(err.message).to.eq('Could not send entire amount')
-      }
+  
+    describe('transfer', () => {
+      it('Should transfer a token', async () => {
+        const computer2 = new Computer()
+        const tbc20 = new TBC20('test', 'TST', sender)
+        const publicKey = tbc20.computer.getPublicKey()
+        await tbc20.mint(publicKey, 200)
+        await sleep(200)
+        await tbc20.transfer(computer2.getPublicKey(), 20)
+        await sleep(200)
+        const res = await tbc20.balanceOf(publicKey)
+        expect(res).to.eq(180)
+      })
+  
+      it('Should transfer random amounts to different people', async () => {
+        const computer2 = new Computer()
+        const computer3 = new Computer()
+        const tbc20 = new TBC20('multiple', 'MULT', sender)
+        const publicKey = tbc20.computer.getPublicKey()
+        await tbc20.mint(publicKey, 200)
+  
+        const amount2 = Math.floor(Math.random() * 100)
+        const amount3 = Math.floor(Math.random() * 100)
+        await sleep(200)
+        await tbc20.transfer(computer2.getPublicKey(), amount2)
+        await sleep(200)
+        await tbc20.transfer(computer3.getPublicKey(), amount3)
+        await sleep(200)
+        const res = await tbc20.balanceOf(publicKey)
+        expect(res).to.eq(200 - amount2 - amount3)
+  
+        const res2 = await tbc20.balanceOf(computer2.getPublicKey())
+        expect(res2).to.eq(amount2)
+  
+        const res3 = await tbc20.balanceOf(computer3.getPublicKey())
+        expect(res3).to.eq(amount3)
+      })
+  
+      it('Should fail if the amount is greater than the balance', async () => {
+        const computer2 = new Computer()
+        const tbc20 = new TBC20('test', 'TST', sender)
+        const publicKey = tbc20.computer.getPublicKey()
+        await tbc20.mint(publicKey, 200)
+        await sleep(200)
+        try {
+          await tbc20.transfer(computer2.getPublicKey(), 201)
+          expect(true).to.eq('false')
+        } catch (err) {
+          expect(err.message).to.eq('Could not send entire amount')
+        }
+      })
     })
   })
 })
