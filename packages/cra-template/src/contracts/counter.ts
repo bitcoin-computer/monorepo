7afbb8131f66f8ff1b2199bbf56d3e9b4e1643b5
jonty007 <vivek.singh2994@gmail.com>
2024-03-21 20:41:27 +0530

    updated cra template

--- packages/cra-template/src/contracts/counter.ts
+++ packages/cra-template/src/contracts/counter.ts
@@ -1,10 +1,11 @@
-
+const { Contract } = await import("@bitcoin-computer/lib")
 export class Counter extends Contract {
   constructor() {
     super({ count: 0 })
   }
 
   inc() {
+    // @ts-ignore
     this.count += 1
   }
 }
