76767fb9ac8e6752646d130f7cef4985fe4bdc26
ltardivo <lauratardivo@gmail.com>
2024-03-27 11:29:27 -0300

    Include comment on how to configure .env in tests files for node local execution

--- packages/TBC721/test/nft.test.ts
+++ packages/TBC721/test/nft.test.ts
@@ -24,6 +24,10 @@ const meta = {
 
 const { expect } = chai
 
+// If you want to connect to your local Bitcoin Computer Node, create a .env file 
+// in the monorepo root level and add the following line:
+// BCN_URL=http://localhost:1031
+
 dotenv.config({ path: '../../.env' })
 
 const url = process.env.BCN_URL
