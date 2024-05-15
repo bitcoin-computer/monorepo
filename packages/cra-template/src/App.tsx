49ca17f92517391898c735ed900cb5a83a3bc9c8
Clemens Ley <ley.clemens@gmail.com>
2024-05-15 16:52:08 -0300

    CRA-template: use computer context

--- packages/cra-template/src/App.tsx
+++ packages/cra-template/src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css"
-import { useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
 import { initFlowbite } from "flowbite"
-import { Auth, Error404, UtilsContext, Wallet, SmartObject, Transaction } from "@bitcoin-computer/components"
+import { Auth, Error404, UtilsContext, Wallet, SmartObject, Transaction, ComputerContext } from "@bitcoin-computer/components"
 import Mint from "./components/Mint"
 import { AllAssets, MyAssets } from "./components/Assets"
 
 export default function App() {
+  const [computer] = useState(Auth.getComputer())
+
   useEffect(() => {
     initFlowbite()
   }, [])
@@ -16,20 +18,22 @@ export default function App() {
     <BrowserRouter>
       <span className="bg-gray-900/50 dark:bg-gray-900/80 z-30 inset-0 sr-only"></span>
       <UtilsContext.UtilsProvider>
-        <Auth.LoginModal />
-        <Wallet />
-        <Navbar />
-        <div className="p-8 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
-          <Routes>
-            <Route path="/" element={<AllAssets />} />
-            <Route path="/mine" element={<MyAssets />} />
-            <Route path="/mint" element={<Mint />} />
-            <Route path="/objects/:rev" element={<SmartObject.Component />} />
-            <Route path="/transactions/:txn" element={<Transaction.Component />} />
-            <Route path="*" element={<Navigate to="/" replace={true} />} />
-            <Route path="*" element={<Error404 />} />
-          </Routes>
-        </div>
+        <ComputerContext.Provider value={computer}>
+          <Auth.LoginModal />
+          <Wallet />
+          <Navbar />
+          <div className="p-8 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+            <Routes>
+              <Route path="/" element={<AllAssets />} />
+              <Route path="/mine" element={<MyAssets />} />
+              <Route path="/mint" element={<Mint />} />
+              <Route path="/objects/:rev" element={<SmartObject.Component />} />
+              <Route path="/transactions/:txn" element={<Transaction.Component />} />
+              <Route path="*" element={<Navigate to="/" replace={true} />} />
+              <Route path="*" element={<Error404 />} />
+            </Routes>
+          </div>
+        </ComputerContext.Provider>
       </UtilsContext.UtilsProvider>
     </BrowserRouter>
   )
