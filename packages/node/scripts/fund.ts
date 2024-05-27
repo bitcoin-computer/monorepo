c57ac7c47d6a5e799cec878cd29dc09325067e91
Clemens Ley <ley.clemens@gmail.com>
2024-05-27 19:21:03 -0300

    Node: remove TEST_ADDRESS from .env file

--- packages/node/scripts/fund.ts
+++ packages/node/scripts/fund.ts
@@ -3,20 +3,18 @@ import { Computer } from '@bitcoin-computer/lib'
 
 dotenv.config()
 
-export async function fund(chain, addressesList: string[] = []) {
-  const addresses = process.env.TEST_ADDRESS.split(';').concat(addressesList)
-
+export async function fund(chain, addresses: string[] = []) {
   const network = (process.env.NETWORK as any) || 'regtest'
   const port = process.env.PORT || 1031
   const url = process.env.BCN_URL || `http://localhost:${port}`
   const computer = new Computer({ chain, network, url })
 
-  // send to address
-  for (let i = 0; i < addresses.length; i += 1)
-    // eslint-disable-next-line no-await-in-loop
-    await computer.rpcCall('generateToAddress', `1 ${addresses[i]}`)
+  // Send to address
+  await Promise.all(
+    addresses.map((address) => computer.rpcCall('generateToAddress', `1 ${address}`)),
+  )
 
-  // generate 100 blocks on top
+  // Mine 100 blocks on top
   await computer.rpcCall('generateToAddress', '100 mrpdUjdfFZQWRYaqgqjgoXTJqn5rwahTHr')
 }
 
