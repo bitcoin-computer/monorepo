83dfc54ec1c949865c873f6aa3518da222c08338
Clemens Ley <ley.clemens@gmail.com>
2024-05-30 22:35:04 -0300

    Fix all but one test

--- packages/node/scripts/fund.ts
+++ packages/node/scripts/fund.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 export async function fund(chain, addresses: string[] = []) {
   const network = (process.env.NETWORK as any) || 'regtest'
-  const port = process.env.PORT || 1031
+  const port = process.env.PORT || '1031'
   const url = process.env.BCN_URL || `http://localhost:${port}`
   const computer = new Computer({ chain, network, url })
 
