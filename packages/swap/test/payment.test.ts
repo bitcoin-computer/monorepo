b9e5fa7d9a59226f646e792c459a6847abf85132
jonty007 <vivek.singh2994@gmail.com>
2024-07-10 20:01:27 +0530

    Payment tests refactor

--- packages/swap/test/payment.test.ts
+++ packages/swap/test/payment.test.ts
@@ -10,38 +10,32 @@ dotenv.config({ path: '../../.env' })
 
 const url = process.env.BCN_URL
 
-describe('Payment', () => {
+describe.only('Payment', () => {
   const alice = new Computer({ url })
-  const bob = new Computer({ url })
 
   before('Before', async () => {
-    await alice.faucet(1e8)
-    await bob.faucet(0.01e8)
+    await alice.faucet(4e8)
   })
 
   describe('Alice creates payment', () => {
     let paymentTxId: string
-    describe('Alice creates an offer and add tx using addSaleTx', async () => {
-      let paymentHelper: PaymentHelper
+    let paymentHelper: PaymentHelper
 
-      before('Before creating an offer', async () => {
-        paymentHelper = new PaymentHelper(alice)
-      })
+    before('Before creating a payment', async () => {
+      paymentHelper = new PaymentHelper(alice)
+    })
 
-      it('Alice deploys the payment contract', async () => {
-        await alice.faucet(0.1e8)
-        await paymentHelper.deploy()
-      })
+    it('Alice deploys the payment contract', async () => {
+      await paymentHelper.deploy()
+    })
 
-      it('Alice creates an payment transaction and broadcast it', async () => {
-        await alice.faucet(1e8)
-        const { tx: paymentTx } = await paymentHelper.createPaymentTx(2e8)
+    it('Alice creates an payment transaction and broadcast it', async () => {
+      const { tx: paymentTx } = await paymentHelper.createPaymentTx(2e8)
 
-        paymentTxId = await alice.broadcast(paymentTx)
+      paymentTxId = await alice.broadcast(paymentTx)
 
-        const payment: Payment = await paymentHelper.getPayment(paymentTxId)
-        expect(payment._amount).eq(2e8)
-      })
+      const payment: Payment = await paymentHelper.getPayment(paymentTxId)
+      expect(payment._amount).eq(2e8)
     })
   })
 })
