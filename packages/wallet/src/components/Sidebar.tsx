4eaf6d8589148706f869118511921095e2f4afab
jonty007 <vivek.singh2994@gmail.com>
2024-07-18 17:06:54 +0530

    Wallet faucet component

--- packages/wallet/src/components/Sidebar.tsx
+++ packages/wallet/src/components/Sidebar.tsx
@@ -138,6 +138,26 @@ export function SideBar() {
                 <span className="flex-1 ms-3 whitespace-nowrap">Details</span>
               </Link>
             </li>
+            {computer.getNetwork() === "regtest" && (
+              <li>
+                <Link
+                  to={`/faucet`}
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  <svg
+                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 20 18"
+                  >
+                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                  </svg>
+                  <span className="flex-1 ms-3 whitespace-nowrap">Faucet</span>
+                </Link>
+              </li>
+            )}
+
             <li>
               <div
                 onClick={Auth.logout}
