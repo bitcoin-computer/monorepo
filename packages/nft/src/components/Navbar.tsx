c98c2fe36f58443d9bddfa486bdfc138d33da55b
jonty007 <vivek.singh2994@gmail.com>
2024-07-23 17:12:06 +0530

    search by public key

--- packages/nft/src/components/Navbar.tsx
+++ packages/nft/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Modal, Auth, UtilsContext, Drawer } from "@bitcoin-computer/components"
 import { useEffect, useState } from "react"
 import { initFlowbite } from "flowbite"
@@ -246,8 +246,17 @@ function NavbarDropdownButton() {
 }
 
 export function Logo({ name = "TBC NFT" }) {
+  const navigate = useNavigate()
+
   return (
-    <Link to={`/`} className="flex items-center space-x-3 rtl:space-x-reverse">
+    <Link
+      to={`/`}
+      onClick={() => {
+        navigate(`/`)
+        window.location.reload()
+      }}
+      className="flex items-center space-x-3 rtl:space-x-reverse"
+    >
       <img src="/logo.png" className="h-10" alt="Bitcoin Computer Logo" />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
         {name}
