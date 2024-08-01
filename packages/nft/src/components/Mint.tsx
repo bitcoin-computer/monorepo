07e0ec0c6974fbf76b55e7e119d2527e72d2a950
Clemens Ley <ley.clemens@gmail.com>
2024-07-31 20:53:17 -0500

    Remove placeholder from mint page

--- packages/nft/src/components/Mint.tsx
+++ packages/nft/src/components/Mint.tsx
@@ -103,8 +103,6 @@ function MintForm(props: {
               value={name}
               onChange={(e) => setName(e.target.value)}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Bitcoin Computer"
-              required
             />
           </div>
           <div>
@@ -117,8 +115,6 @@ function MintForm(props: {
               value={symbol}
               onChange={(e) => setSymbol(e.target.value)}
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="TBC"
-              required
             />
           </div>
           <div>
