22429ab8f50b519bdf677cbf71b042c66e65b793
Clemens Ley <ley.clemens@gmail.com>
2024-08-13 14:29:10 -0500

    TBC20: refactor smart contract and test

--- packages/TBC20/src/token.ts
+++ packages/TBC20/src/token.ts
@@ -1,5 +1,4 @@
 /* eslint-disable max-classes-per-file */
-
 import { Contract } from '@bitcoin-computer/lib'
 
 export class Token extends Contract {
@@ -20,56 +19,61 @@ export class Token extends Contract {
 }
 
 export interface ITBC20 {
-  mint(publicKey: string, amount: number): Promise<string>
-  totalSupply(): Promise<number>
-  balanceOf(publicKey: string): Promise<number>
-  transfer(to: string, amount: number): Promise<void>
+  deploy(): Promise<string>
+  mint(publicKey: string, amount: number, name: string, symbol: string): Promise<string>
+  totalSupply(root: string): Promise<number>
+  balanceOf(publicKey: string, root: string): Promise<number>
+  transfer(to: string, amount: number, root: string): Promise<void>
 }
 
 export class TBC20 implements ITBC20 {
   name: string
   symbol: string
   computer: any
-  mintId: string
+  mod: string
 
-  constructor(name: string, symbol: string, computer: any, mintId?: string) {
-    this.name = name
-    this.symbol = symbol
+  constructor(computer: any, mod?: string) {
     this.computer = computer
-    this.mintId = mintId
+    this.mod = mod
   }
 
-  async mint(publicKey: string, amount: number): Promise<string> {
-    const args = [publicKey, amount, this.name, this.symbol]
-    const token = await this.computer.new(Token, args)
-    this.mintId = token._root
-    return this.mintId
+  async deploy() {
+    this.mod = (await this.computer.deploy(`export ${Token}`))
+    return this.mod
   }
 
-  async totalSupply(): Promise<number> {
-    if (!this.mintId) throw new Error('Please set a mint id.')
-    const rootBag = await this.computer.sync(this.mintId)
+  async mint(
+    publicKey: string,
+    amount: number,
+    name: string,
+    symbol: string
+  ): Promise<string | undefined> {
+    const args = [publicKey, amount, name, symbol]
+    const token = await this.computer.new(Token, args, this.mod)
+    return token._root
+  }
+
+  async totalSupply(root: string): Promise<number> {
+    const rootBag = await this.computer.sync(root)
     return rootBag.tokens
   }
 
-  private async getBags(publicKey): Promise<Token[]> {
-    if (!this.mintId) throw new Error('Please set a mint id.')
-    const revs = await this.computer.query({ publicKey })
+  private async getBags(publicKey: string, root: string): Promise<Token[]> {
+    const revs = await this.computer.query({ publicKey, mod: this.mod })
     const bags = await Promise.all(revs.map(async (rev: string) => this.computer.sync(rev)))
-    return bags.flatMap((bag: Token & { _root: string }) =>
-      bag._root === this.mintId ? [bag] : []
-    )
+    return bags.flatMap((bag: Token & { _root: string }) => (bag._root === root ? [bag] : []))
   }
 
-  async balanceOf(publicKey: string): Promise<number> {
-    const bags = await this.getBags(publicKey)
+  async balanceOf(publicKey: string, root: string): Promise<number> {
+    if (typeof root === 'undefined') throw new Error('Please pass a root into balanceOf.')
+    const bags = await this.getBags(publicKey, root)
     return bags.reduce((prev, curr) => prev + curr.tokens, 0)
   }
 
-  async transfer(to: string, amount: number): Promise<void> {
+  async transfer(to: string, amount: number, root: string): Promise<void> {
     let _amount = amount
     const owner = this.computer.getPublicKey()
-    const bags = await this.getBags(owner)
+    const bags = await this.getBags(owner, root)
     const results = []
     while (_amount > 0 && bags.length > 0) {
       const [bag] = bags.splice(0, 1)
