4805333e6919416c128890a9b8d10dfe449eed91
Clemens Ley <ley.clemens@gmail.com>
2024-08-22 22:00:51 -0500

    FT2: fix dark mode

--- packages/ft2/src/components/Orders.tsx
+++ packages/ft2/src/components/Orders.tsx
@@ -8,15 +8,15 @@ export default function Orders() {
 
   return <div className="container mx-auto p-6">
     <div className="grid grid-cols-2 gap-4">
-        <div className="bg-white p-6 rounded shadow">
+        <div className="bg-white dark:bg-gray-800 p-6 rounded shadow">
           <h4 className="text-2xl mb-2 font-bold dark:text-white">Sell Orders</h4>
           <SellOrderForm computer={computer} />
           <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
           <SellOrders computer={computer} />
         </div>
 
-        <div className="bg-white p-6 rounded shadow">
-        <h4 className="text-2xl mb-2 font-bold dark:text-white">Buy Orders</h4>
+        <div className="bg-white dark:bg-gray-800 p-6 rounded shadow">
+          <h4 className="text-2xl mb-2 font-bold dark:text-white">Buy Orders</h4>
           <BuyOrderForm computer={computer} />
           <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
           <BuyOrders computer={computer} />
