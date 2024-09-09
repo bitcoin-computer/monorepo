dd3458be71c68ad85547a4640d364bca3ca15a39
jonty007 <vivek.singh2994@gmail.com>
2024-09-09 18:31:35 +0530

    test-unit removed as they are async tests, build of TBC20

---
+++ packages/TBC20/build/src/token.d.ts
@@ -0,0 +1,30 @@
+declare const Contract: typeof import("@bitcoin-computer/lib/computer").Contract;
+export declare class Token extends Contract {
+    amount: number;
+    name: string;
+    symbol: string;
+    _owners: string[];
+    constructor(to: string, amount: number, name: string, symbol?: string);
+    transfer(to: string, amount?: number): Token | null;
+}
+export interface ITBC20 {
+    deploy(): Promise<string>;
+    mint(publicKey: string, amount: number, name: string, symbol: string): Promise<string>;
+    totalSupply(root: string): Promise<number>;
+    balanceOf(publicKey: string, root: string): Promise<number>;
+    transfer(to: string, amount: number, root: string): Promise<void>;
+}
+export declare class TBC20 implements ITBC20 {
+    name: string;
+    symbol: string;
+    computer: any;
+    mod: string;
+    constructor(computer: any, mod?: string);
+    deploy(): Promise<string>;
+    mint(publicKey: string, amount: number, name: string, symbol: string): Promise<string | undefined>;
+    totalSupply(root: string): Promise<number>;
+    private getBags;
+    balanceOf(publicKey: string, root: string): Promise<number>;
+    transfer(to: string, amount: number, root: string): Promise<void>;
+}
+export {};
