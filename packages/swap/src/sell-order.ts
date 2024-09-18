9cc840c5674b827f7faecf13c9cd13237d568e99
Clemens Ley <ley.clemens@gmail.com>
2024-09-18 17:50:43 -0500

    Swap: add sell-order contract

---
+++ packages/swap/src/sell-order.ts
@@ -0,0 +1,79 @@
+/* eslint max-classes-per-file: ["error", 2] */
+import { Transaction } from '@bitcoin-computer/lib'
+import { OfferHelper } from './offer.js'
+import { Payment, PaymentHelper, PaymentMock } from './payment.js'
+import { SaleHelper } from './sale.js'
+
+export class SellOrder extends Contract {
+  txHex: string
+
+  constructor(owner: string, txHex?: string) {
+    super({ _owners: [owner], txHex })
+  }
+}
+
+export class SellOrderHelper {
+  computer: any
+  mod?: string
+  saleHelper: SaleHelper
+  offerHelper: OfferHelper
+  paymentHelper: PaymentHelper
+
+  constructor(
+    computer: any,
+    saleMod: string,
+    offerMod: string,
+    paymentMod: string,
+    sellMod?: string
+  ) {
+    this.computer = computer
+    this.saleHelper = new SaleHelper(computer, saleMod)
+    this.offerHelper = new OfferHelper(computer, offerMod)
+    this.paymentHelper = new PaymentHelper(computer, paymentMod)
+    this.mod = sellMod
+  }
+
+  async deploy(): Promise<string> {
+    this.mod = await this.computer.deploy(`export ${SellOrder}`)
+    return this.mod
+  }
+
+  async broadcastSellOrder(amount: number, tokenRev: string): Promise<string> {
+    const mock = new PaymentMock(amount)
+    const { tx: saleTx } = await this.saleHelper.createSaleTx({ _rev: tokenRev }, mock)
+    const publicKey = this.computer.getPublicKey()
+    const url = this.computer.getUrl()
+    const { tx: offerTx } = await this.offerHelper.createOfferTx(publicKey, url, saleTx)
+    return this.computer.broadcast(offerTx)
+  }
+
+  async getSaleTx(sellOrderRev: string) {
+    const { txHex: saleTxHex } = (await this.computer.sync(sellOrderRev)) as any
+    return Transaction.deserialize(saleTxHex)
+  }
+
+  async parseSellOrder(
+    sellOrderRev: string
+  ): Promise<{ saleTx: any; price: number; open: boolean; token: any }> {
+    const saleTx = await this.getSaleTx(sellOrderRev)
+    if (!(await this.saleHelper.isSaleTx(saleTx))) return {} as any
+    const price = await this.saleHelper.checkSaleTx(saleTx)
+    const { env } = await this.computer.decode(saleTx)
+    const tokenRev = env.o
+    const open = await this.computer.isUnspent(tokenRev)
+    const token = await this.computer.sync(tokenRev)
+    return { saleTx, price, open, token }
+  }
+
+  async settleSellOrder(price: number, deserialized: Transaction) {
+    const payment = await this.computer.new(Payment, [price], this.paymentHelper.mod)
+    const finalTx = SaleHelper.finalizeSaleTx(
+      deserialized,
+      payment,
+      this.computer.toScriptPubKey()!
+    )
+    await this.computer.fund(finalTx)
+    await this.computer.sign(finalTx)
+    return this.computer.broadcast(finalTx)
+  }
+}
---
+++ packages/swap/test/sell-order.test.ts
@@ -0,0 +1,59 @@
+import { expect } from 'chai'
+import dotenv from 'dotenv'
+import { Token } from '@bitcoin-computer/TBC20'
+import { Computer } from '@bitcoin-computer/lib'
+import { SellOrderHelper } from '../src/sell-order'
+import { OfferHelper, PaymentHelper, SaleHelper } from '../src'
+
+dotenv.config({ path: '../../.env' })
+
+describe.only('SellOrder', () => {
+  const nftPrice = 1e8
+  const fee = 100000
+
+  describe('Buying a Token', () => {
+    it('Should work with helper classes', async () => {
+      const p = 10000
+
+      const buyer = new Computer()
+      await buyer.faucet(nftPrice + fee)
+      const seller = new Computer()
+      await seller.faucet(nftPrice + fee)
+
+      // Deploy smart contracts
+      const saleHelperS = new SaleHelper(seller)
+      const saleMod = await saleHelperS.deploy()
+      const offerHelperS = new OfferHelper(seller)
+      const offerMod = await offerHelperS.deploy()
+      const paymentHelperS = new PaymentHelper(seller)
+      const paymentMod = await paymentHelperS.deploy()
+      const sellOrderHelperS = new SellOrderHelper(seller, saleMod, offerMod, paymentMod)
+      await sellOrderHelperS.deploy()
+
+      // Seller deploys the SellOrder contract and creates an Token
+      const t = await seller.new(Token, [seller.getPublicKey(), 100, 'a', 'AAA'])
+
+      // Seller creates an order
+      const txId = await sellOrderHelperS.broadcastSellOrder(p, t._rev)
+      expect(txId).a('string')
+
+      const { res: offer } = (await seller.sync(txId)) as any
+      expect(offer && offer.txHex && typeof offer.txHex === 'string').eq(true)
+      expect(await seller.isUnspent(t._rev)).eq(true)
+
+      // Buyer closes the order
+      const sellOrderHelperB = new SellOrderHelper(buyer, saleMod, offerMod, paymentMod)
+      const res = await sellOrderHelperB.parseSellOrder(`${txId}:0`)
+      expect(Object.keys(res)).deep.eq(['saleTx', 'price', 'open', 'token'])
+      const { saleTx, price, open, token } = res
+      expect(typeof saleTx).eq('object')
+      expect(price).eq(p)
+      expect(open).eq(true)
+      expect(token).deep.eq(t)
+
+      // Seller settles the order
+      const saleTxId = await sellOrderHelperS.settleSellOrder(price, saleTx)
+      expect(saleTxId).a('string')
+    })
+  })
+})
