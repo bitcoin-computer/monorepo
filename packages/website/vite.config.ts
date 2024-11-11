73bad4cecf3319ed9bf55af1f057dac13f0d05b9
jonty007 <vivek.singh2994@gmail.com>
2024-11-11 21:55:56 +0530

    Update website to use vite

---
+++ packages/website/vite.config.ts
@@ -0,0 +1,29 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
+import svgr from "vite-plugin-svgr"
+import path from "path"
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  return {
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        // Define the alias pointing to the specific entry point in node_modules
+        "@bitcoin-computer/lib": path.resolve(__dirname, "../lib/dist/bc-lib.browser.min.mjs")
+      }
+    },
+    server: {
+      port: parseInt(env.VITE_PORT)
+    },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./src/setupTests.ts"]
+    }
+  }
+})
