902eeea21406b6d7ad3cab753dd9da4a165a2ecf
Clemens Ley <ley.clemens@gmail.com>
2024-11-12 13:58:16 -0600

    Built files

--- packages/nakamotojs/src/transaction.d.ts
+++ packages/nakamotojs/src/transaction.d.ts
@@ -23,13 +23,13 @@ export declare class Transaction {
     static readonly SIGHASH_INPUT_MASK = 128;
     static readonly ADVANCED_TRANSACTION_MARKER = 0;
     static readonly ADVANCED_TRANSACTION_FLAG = 1;
+    ins: Input[];
+    outs: Output[];
+    version: number;
+    locktime: number;
     static fromBuffer(buffer: Buffer, _NO_STRICT?: boolean): Transaction;
     static fromHex(hex: string): Transaction;
     static isCoinbaseHash(buffer: Buffer): boolean;
-    version: number;
-    locktime: number;
-    ins: Input[];
-    outs: Output[];
     isCoinbase(): boolean;
     addInput(hash: Buffer, index: number, sequence?: number, scriptSig?: Buffer): number;
     updateInput(inputIndex: number, opts: {
