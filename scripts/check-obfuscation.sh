52d95999c0a3ed707b08f2a7f0cc5ffbfd605fc3
jonty007 <vivek.singh2994@gmail.com>
2024-11-12 20:56:47 +0530

    fix deprecated dependencies

--- scripts/check-obfuscation.sh
+++ scripts/check-obfuscation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of folders to skip
-skip_folders=("vite-template" "nft-vite" "explorer-vite" "wallet-vite" "chat-vite")
+skip_folders=("vite-template" "nft" "explorer" "wallet" "chat" "ft2" "website")
 
 # Check if the obfuscation was successful on all dist folders
 msg="Checking obfuscation ..."
