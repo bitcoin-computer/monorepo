40a41e20cefc2ad0fa03937c3b3f0ac2af0f4858
ltardivo <lauratardivo@gmail.com>
2024-11-15 21:29:41 -0300

    Remove commented lines

--- packages/node/scripts/up.ts
+++ packages/node/scripts/up.ts
@@ -1,29 +1,7 @@
 #!/usr/bin/env node
 import { ArgumentParser } from 'argparse'
-import { availableParallelism } from 'os';
 import { execSync } from 'child_process'
 import 'dotenv/config'
-
-// function runSync(commandLine) {
-//   // let numWorkers = Math.max(Math.floor(require('os').cpus().length - 2), 1); // Reserve 2 CPUs
-//   let numWorkers = Math.max(Math.floor(availableParallelism() - 2), 1); // Reserve 2 CPUs availableParallelism()
-//   if (numWorkers < 1) {
-//     console.log('Warning: No CPUs available for workers.')
-//     numWorkers = 2
-//   }
-//   // TODO REMOVE
-//   numWorkers = 4
-//   console.log(`Launching ${numWorkers} workers.`);
-//   let workersCommand = commandLine
-//   for (let workerId = 0; workerId < numWorkers; workerId++) {
-//     workersCommand += ` && export BCN_WORKER_ID=${workerId + 1} BCN_NUM_WORKERS=${numWorkers}; docker compose -f docker-compose.yml run -d sync`;
-//   }
-//   console.log(workersCommand)
-//   const parallelCommand = workersCommand;
-//   // spawn('sh', ['-c', parallelCommand]);
-//   execSync(`sh -c "${parallelCommand}"`, { stdio: 'inherit' })
-// }
-
 
 function main() {
   if (!process.env) {
@@ -55,21 +33,6 @@ function main() {
   const selectedService = args.db || args.bcn || args.node || args.sync
   
   const commandLine = `docker compose -f docker-compose.yml `
-
-  // if (selectedService) {
-  //   // Run the selected service. If -sync is selected, run the sync service with the number of available cores
-  //   if(selectedService === 'sync') {
-  //     runSync(commandLine)
-  //   } else {
-  //     console.log(`Launching all services ${commandLine}`)
-  //     execSync(`sh -c "${commandLine} up ${selectedService}"`, { stdio: 'inherit' })
-  //   }
-  // } else {
-    // Run all services. Launch bcn + node + db and then spawn sync processes
-    //execSync(`sh -c "${commandLine} up bcn node db"`, { stdio: 'inherit' })
-    // runSync(commandLine)
-  // }
-
   execSync(`sh -c "${commandLine} up ${selectedService || ''}"`, { stdio: 'inherit' })
 }
 
