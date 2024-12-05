e2e54ff02d4a615473b62b6583d4cfc7ef753e9a
jonty007 <vivek.singh2994@gmail.com>
2024-12-05 14:38:10 +0530

    Add scripts and clean up obfuscation script

--- scripts/check-obfuscation.sh
+++ scripts/check-obfuscation.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # List of folders to skip
-skip_folders=("vite-template" "nft" "explorer" "wallet" "chat" "website" "chess-app")
+check_folders=("lib" "node")
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Define the base directory for the packages
+packages_dir="$script_dir/../packages"
 
 # Check if the obfuscation was successful on all dist folders
 msg="Checking obfuscation ..."
-for folder in $(find ./packages -type d -name dist -maxdepth 2); do
+for folder in "$packages_dir/lib/dist" "$packages_dir/node/dist"; do
   # Extract the parent folder name
   parent_folder=$(basename "$(dirname "$folder")")
-  # Check if the parent folder is in the skip list
-  if [[ " ${skip_folders[@]} " =~ " ${parent_folder} " ]]; then
-    echo "Skipping obfuscation check for $parent_folder"
-    continue
-  fi
 
+  echo "Checking $parent_folder"
   for file in $folder/*.*; do
     # search for two spaces on the first bytes of the file
     head -c 1024 "$file" | grep -q '  ' && echo "Obfuscation failed for $file" && exit 1
