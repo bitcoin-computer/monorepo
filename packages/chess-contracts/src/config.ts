c0b9de0869f4a3b37f98f735a7d00c5750cefc13
Clemens Ley <ley.clemens@gmail.com>
2025-01-02 23:07:51 -0300

    Chess-Contract: refactor config.ts

--- packages/chess-contracts/src/config.ts
+++ packages/chess-contracts/src/config.ts
@@ -3,36 +3,19 @@
 import 'dotenv/config'
 
 let VITE_API_BASE_URL: string | undefined
-let VITE_CHESS_GAME_MOD_SPEC: string | undefined
-let VITE_CHAIN: string | undefined
-let VITE_NETWORK: string | undefined
-let VITE_URL: string | undefined
-let VITE_MNEMONIC: string | undefined
 
 // Vite environment
 if (import.meta.env && import.meta.env.MODE) {
   VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL
-  VITE_CHESS_GAME_MOD_SPEC = import.meta.env.VITE_CHESS_GAME_MOD_SPEC
-  VITE_CHAIN = import.meta.env.VITE_CHAIN
-  VITE_NETWORK = import.meta.env.VITE_NETWORK
-  VITE_URL = import.meta.env.VITE_URL
-  VITE_MNEMONIC = import.meta.env.VITE_MNEMONIC
-
 // Node.js environment
 } else if (typeof process === 'object' && process.versions && process.versions.node) {
   VITE_API_BASE_URL = process.env.VITE_API_BASE_URL
-  VITE_CHESS_GAME_MOD_SPEC = process.env.VITE_CHESS_GAME_MOD_SPEC
-  VITE_CHAIN = process.env.VITE_CHAIN
-  VITE_NETWORK = process.env.VITE_NETWORK
-  VITE_URL = process.env.VITE_URL
-  VITE_MNEMONIC = process.env.VITE_MNEMONIC
-
 } else {
   throw new Error('Unsupported execution environment.')
 }
 
-const vars = [VITE_API_BASE_URL, VITE_CHESS_GAME_MOD_SPEC, VITE_CHAIN, VITE_NETWORK, VITE_URL]
+const vars = [VITE_API_BASE_URL]
 
 if (vars.some((el) => el === undefined)) throw new Error(`Please create a .env file ${vars}`)
 
-export { VITE_API_BASE_URL, VITE_CHESS_GAME_MOD_SPEC, VITE_CHAIN, VITE_NETWORK, VITE_URL, VITE_MNEMONIC }
+export { VITE_API_BASE_URL }
