af80071cdb271f79adfe4e94f43a447567dc7176
jonty007 <vivek.singh2994@gmail.com>
2025-02-03 12:56:47 +0530

    Removed unnecessary expects

--- packages/vite-template/tests/browser.test.ts
+++ packages/vite-template/tests/browser.test.ts
@@ -47,19 +47,16 @@ describe('Bitcoin Computer Tests', () => {
     await delay(2000)
     await page.waitForSelector('span.loading-smart-contract-span', { hidden: true })
     const signInButton = await findButtonByText(page, 'Sign in')
-    expect(signInButton).to.not.be.null
     await signInButton!.click()
 
     await page.waitForSelector('button[type="submit"]', { visible: true })
 
     const loginButton = await findButtonByText(page, 'Log In')
-    expect(loginButton).to.not.be.null
     await loginButton!.click()
 
     await page.waitForSelector('button[data-drawer-target="wallet-drawer"]', { visible: true })
 
     const walletButton = await findButtonByText(page, 'Wallet')
-    expect(walletButton).to.not.be.null
     await walletButton!.click()
     await delay(1000)
   })
@@ -67,13 +64,11 @@ describe('Bitcoin Computer Tests', () => {
   it('should click the "Wallet" button, and fund', async () => {
     await page.waitForSelector('#fund-wallet', { visible: true })
     const fundButton = await findButtonByText(page, 'Fund')
-    expect(fundButton).to.not.be.null
     await fundButton!.click()
 
     await page.waitForSelector('button[data-drawer-hide="wallet-drawer"]', { visible: true })
 
     const closeButton = await page.$('button[data-drawer-hide="wallet-drawer"]')
-    expect(closeButton).to.not.be.null
     await closeButton!.click()
     await delay(1000)
   })
@@ -81,20 +76,15 @@ describe('Bitcoin Computer Tests', () => {
   it('should mint a counter and navigate to counter page', async () => {
     await page.waitForSelector('#mint-button', { visible: true, hidden: false })
     const mintButton = await page.$('#mint-button')
-    expect(mintButton).to.not.be.null
     await mintButton!.click()
 
     await page.waitForSelector('#mint-counter-button', { visible: true, hidden: false })
     const mintCounterButton = await await page.$('#mint-counter-button')
-    expect(mintCounterButton).to.not.be.null
     await mintCounterButton!.click()
 
     await page.waitForSelector('#mint-success', { visible: true, hidden: false })
-    const successDiv = await page.$('#mint-success')
-    expect(successDiv).to.not.be.null
-
+    await page.waitForSelector('#counter-link', { visible: true, hidden: false })
     const counterLink = await page.$('#counter-link')
-    expect(counterLink).to.not.be.null
     await counterLink!.click()
 
     await delay(1000)
@@ -102,21 +92,13 @@ describe('Bitcoin Computer Tests', () => {
 
   it('should input 2, open dropdown, and select "number"', async () => {
     await page.waitForSelector('#property-count-value', { visible: true, hidden: false })
-    const preElement = await page.$('#property-count-value')
-    expect(preElement).to.not.be.null
-
     await page.waitForSelector('#function-inc', { visible: true, hidden: false })
-    const functionIncDiv = await page.$('#function-inc')
-    expect(functionIncDiv).to.not.be.null
-
     await page.waitForSelector('#inc-num', { visible: true, hidden: false })
     const inputField = await page.$('#inc-num')
-    expect(inputField).to.not.be.null
     await inputField!.click({ clickCount: 3 })
     await inputField!.type('2')
 
     const dropdownButton = await page.$('#dropdownButtonincnum')
-    expect(dropdownButton).to.not.be.null
     await dropdownButton!.click()
 
     await page.waitForSelector('#dropdownMenuincnum', { visible: true })
@@ -127,7 +109,6 @@ describe('Bitcoin Computer Tests', () => {
       )
     })
 
-    expect(numberOption).to.not.be.null
     await numberOption!.click()
 
     await delay(1000)
@@ -136,25 +117,19 @@ describe('Bitcoin Computer Tests', () => {
   it('should call the counter function and increment the counter by 2', async () => {
     await page.waitForSelector('#inc-call-function-button', { visible: true, hidden: false })
     const callFunctionButton = await page.$('#inc-call-function-button')
-    expect(callFunctionButton).to.not.be.null
     await callFunctionButton!.click()
 
     await page.waitForSelector('#smart-call-execution-success', { visible: true, hidden: false })
-    const successDiv = await page.$('#smart-call-execution-success')
-    expect(successDiv).to.not.be.null
-
     await page.waitForSelector('#smart-call-execution-counter-link', {
       visible: true,
       hidden: false,
     })
     const counterLink = await page.$('#smart-call-execution-counter-link')
-    expect(counterLink).to.not.be.null
     await counterLink!.click()
     await delay(1000)
 
     await page.waitForSelector('#property-count-value', { visible: true, hidden: false })
     const preElement = await page.$('#property-count-value')
-    expect(preElement).to.not.be.null
     const preText = await page.evaluate((el) => el.textContent?.trim(), preElement)
     expect(preText).to.equal('2')
     await delay(1000)
@@ -162,18 +137,13 @@ describe('Bitcoin Computer Tests', () => {
 
   it('should successfully transfer the counter to a public key', async () => {
     await page.waitForSelector('#function-transfer', { visible: true, hidden: false })
-    const functionTransferDiv = await page.$('#function-transfer')
-    expect(functionTransferDiv).to.not.be.null
-
     await page.waitForSelector('#transfer-publicKey', { visible: true, hidden: false })
     const inputField = await page.$('#transfer-publicKey')
-    expect(inputField).to.not.be.null
     await inputField!.click({ clickCount: 3 })
     await inputField!.type('0363f42382c95e5489b87c6b385df8711fd4908bbdc1820b33aab5d9f2b3a731b0')
 
     await page.waitForSelector('#dropdownButtontransferpublicKey', { visible: true, hidden: false })
     const dropdownButton = await page.$('#dropdownButtontransferpublicKey')
-    expect(dropdownButton).to.not.be.null
     await dropdownButton!.click()
 
     await page.waitForSelector('#dropdownMenutransferpublicKey', { visible: true })
@@ -183,17 +153,17 @@ describe('Bitcoin Computer Tests', () => {
       )
     })
 
-    expect(stringOption).to.not.be.null
     await stringOption!.click()
 
     await page.waitForSelector('#transfer-call-function-button', { visible: true, hidden: false })
     const callFunctionButton = await page.$('#transfer-call-function-button')
-    expect(callFunctionButton).to.not.be.null
     await callFunctionButton!.click()
 
     await page.waitForSelector('#smart-call-execution-success', { visible: true, hidden: false })
     const successDiv = await page.$('#smart-call-execution-success')
-    expect(successDiv).to.not.be.null
+    if (!successDiv) {
+      throw new Error('Counter transfer unsuccessful')
+    }
     await delay(1000)
   })
 })
