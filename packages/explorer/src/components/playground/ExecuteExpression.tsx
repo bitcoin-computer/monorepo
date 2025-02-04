642673e7bc3a8fd5e59870668673ff680bc83168
Clemens Ley <ley.clemens@gmail.com>
2025-02-04 12:22:07 -0300

    Fix all type errors

--- packages/explorer/src/components/playground/ExecuteExpression.tsx
+++ packages/explorer/src/components/playground/ExecuteExpression.tsx
@@ -172,7 +172,6 @@ const ExecuteExpression = (props: {
 
       <button
         type="button"
-        onClick={handleExpressionCall}
         disabled={isCallDisabled}
         onClick={handleExpressionCall}
         className={`text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:ring-4 focus:outline-none
