c75a0eca6c309650f543a486e598a09670cd5079
jonty007 <vivek.singh2994@gmail.com>
2025-02-13 14:17:22 +0530

    update utxos page

--- packages/explorer/src/components/Utxos.tsx
+++ packages/explorer/src/components/Utxos.tsx
@@ -44,51 +44,61 @@ const UTXODisplay = () => {
     <>
       {utxos && utxos.length > 0 && (
         <>
-          <div className="p-4 rounded-3xl shadow-md bg-white dark:bg-gray-800 transition-colors">
-            <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
-              Address: {address}
-            </h2>
-            <p className="text-m text-gray-700 dark:text-gray-300 mt-2">
-              <span className="font-medium">Total Amount:</span> {totalAmount} {computer.getChain()}
+          <div className="w-full">
+            <h1 className="mb-2 text-5xl font-extrabold dark:text-white">Address</h1>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+              {address}
             </p>
           </div>
-          <div className="relative overflow-x-auto">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    TXID
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    VOUT
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Amount ({computer.getChain()})
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {utxos.map((utxo, index) => (
-                  <tr
-                    key={index}
-                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-                  >
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                    >
-                      {utxo.rev?.split(':')[0]}
+
+          <div className="w-full">
+            <h2 className="mb-2 text-4xl font-bold dark:text-white">Balance</h2>
+            <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+              {totalAmount} {computer.getChain()}
+            </p>
+          </div>
+
+          <div>
+            <h2 className="mb-2 text-4xl font-bold dark:text-white">UTXOs</h2>
+
+            <div className="relative overflow-x-auto">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      TXID
                     </th>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
-                      {utxo.vout}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white font-bold">
-                      {utxo.satoshis / 1e8}
-                    </td>
+                    <th scope="col" className="px-6 py-3">
+                      VOUT
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Amount ({computer.getChain()})
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {utxos.map((utxo, index) => (
+                    <tr
+                      key={index}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+                    >
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                      >
+                        {utxo.rev?.split(':')[0]}
+                      </th>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                        {utxo.vout}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-white font-bold">
+                        {utxo.satoshis / 1e8}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </>
       )}
