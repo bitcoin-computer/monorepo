42f547786b570efec00c95fa64aba8493b707b68
jonty007 <vivek.singh2994@gmail.com>
2025-03-04 20:59:42 +0530

    removed the alias for bitcoin-computer lib in vite template

--- packages/vite-template/vite.puppeteer.config.ts
+++ packages/vite-template/vite.puppeteer.config.ts
@@ -3,25 +3,18 @@
 
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    resolve: {
-      alias: {
-        // Define the alias pointing to the specific entry point in node_modules
-        '@bitcoin-computer/lib': path.resolve(__dirname, '../lib/dist/bc-lib.browser.min.mjs'),
-      },
-    },
     server: {
       port: parseInt(env.VITE_PORT),
       headers: {
-        "Cross-Origin-Opener-Policy": "same-origin",
-        "Cross-Origin-Embedder-Policy": "require-corp"
-      }
+        'Cross-Origin-Opener-Policy': 'same-origin',
+        'Cross-Origin-Embedder-Policy': 'require-corp',
+      },
     },
     test: {
       include: ['tests/**/*.test.ts'],
