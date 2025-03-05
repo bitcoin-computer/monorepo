9f4fd62c61d387b6baf007a74b713ea788f1235c
jonty007 <vivek.singh2994@gmail.com>
2025-03-05 22:04:53 +0530

    support search for address and puppeteer test in test:all command

--- packages/vite-template/vite.config.ts
+++ packages/vite-template/vite.config.ts
@@ -1,12 +1,10 @@
-/// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     server: {
@@ -16,11 +14,5 @@ export default defineConfig(({ mode }) => {
         'Cross-Origin-Embedder-Policy': 'require-corp',
       },
     },
-    test: {
-      globals: true,
-      include: ['src/**/*.test.tsx'],
-      environment: 'jsdom',
-      setupFiles: ['./src/setupTests.ts'],
-    },
-  }
-})
+  };
+});
