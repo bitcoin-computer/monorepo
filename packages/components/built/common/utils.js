bf67fbc78c430211311b3ec601bd94c153113266
ltardivo <lauratardivo@gmail.com>
2025-04-01 19:19:18 -0300

    Fixes str2BigInt function and tests

--- packages/components/built/common/utils.js
+++ packages/components/built/common/utils.js
@@ -59,9 +59,14 @@ export function bigInt2Str(a) {
     return `${integerPart}.${fractionalPart || '0'}`;
 }
 export function str2BigInt(a) {
+    // Validate number contains at most one dot and is not empty
+    if ((a.match(/\./g) || []).length > 1 || a === '.' || a === '') {
+        throw new Error('Invalid number');
+    }
     const [integerPart, fractionalPart = ''] = a.split('.');
-    if (!/^\d*$/.test(integerPart)) {
-        throw new Error('Invalid integer part. Please enter a valid number.');
+    // Validate integer and fractional part contains only digits (or is empty)
+    if (!/^\d*$/.test(integerPart) || !/^\d*$/.test(fractionalPart)) {
+        throw new Error('Invalid number');
     }
     const paddedFractionalPart = fractionalPart.padEnd(8, '0').slice(0, 8);
     const totalSatoshisStr = integerPart + paddedFractionalPart;
