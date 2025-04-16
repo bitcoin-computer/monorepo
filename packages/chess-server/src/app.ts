07da3a8d7d9ef2fe7ff2d6a9ef54f5fc105bd4f4
jonty007 <vivek.singh2994@gmail.com>
2025-04-16 20:54:16 +0530

    Remove DB from server

--- packages/chess-server/src/app.ts
+++ packages/chess-server/src/app.ts
@@ -1,57 +1,67 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import express, { Request, Response } from 'express'
-import { db } from '../db/db.js'
-import { Computer } from '@bitcoin-computer/lib'
-import { crypto } from '@bitcoin-computer/nakamotojs'
-import { randomBytes } from 'node:crypto'
+import { Computer, Transaction } from '@bitcoin-computer/lib'
 import cors from 'cors'
-import { Chess } from '@bitcoin-computer/chess-contracts'
+import { Chess, ChessContractHelper } from '@bitcoin-computer/chess-contracts'
+import { script as bscript, networks } from '@bitcoin-computer/nakamotojs'
+import { ECPairFactory } from 'ecpair'
+import * as ecc from '@bitcoin-computer/secp256k1'
+const ECPair = ECPairFactory(ecc)
 
 const app = express()
 const PORT = 4000
-const computer = new Computer({ chain: 'LTC', network: 'regtest', url: 'http://127.0.0.1:1031' })
+// Move this to .env
+const operatorMnemonic = `witness ball town vast limit abstract proof clay castle lens year protect`
+const computer = new Computer({
+  chain: 'LTC',
+  network: 'regtest',
+  url: 'http://127.0.0.1:1031',
+  mnemonic: operatorMnemonic
+})
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/hash', async (_: Request, res: Response) => {
+app.post('/claim-win/:id', async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { redeemTxHex } = req.body
   try {
-    const secret = randomBytes(32).toString('hex')
-    const buffer = Buffer.from(secret)
-    const hash = crypto.sha256(crypto.sha256(buffer)).toString('hex')
-    await db.none(
-      `INSERT INTO "Secrets" ("secret", "hash") VALUES ($1, $2);`,
-      [secret, hash]
-    )
-    res.status(200).json(hash)
-  } catch (error) {
-    if (error instanceof Error)
-      res.status(500).json({ error: `Internal server error: ${error.message}` })
-  }
-})
-
-app.get('/secret/:id', async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params    
-    const [rev] = await computer.query({ ids: [id] })
-    const contract = await computer.sync(rev) as any
+    const [rev] = await computer.query({ ids: [id + ':0'] })
+    const contract = (await computer.sync(rev)) as any
     const game = new Chess(contract.fen)
-
-    if (game.isGameOver()) {
-      const winner = contract._owners[0]
-      const hash = winner === contract.publicKeyW ? contract.secretHashW : contract.secretHashB
-
-      const { secret } = await db.one(
-        `SELECT secret FROM "Secrets" WHERE "hash"=$1;`,
-        [hash]
-      )
-      res.status(200).json(secret)
-    } else {
-      res.json('')
+    if (!game.isGameOver()) {
+      res.status(400).json({ message: 'Cannot claim as the game is not over!' })
+      return
     }
+
+    const winnerPublicKey = contract._owners[0] as string
+    const network = computer.getNetwork()
+    const chain = computer.getChain()
+    const NETWORKOBJ = networks.getNetwork(chain, network)
+    const { privateKey: operatorPrivateKey } = computer.wallet
+    const operatorKeyPair = ECPair.fromPrivateKey(operatorPrivateKey, { network: NETWORKOBJ })
+
+    const redeemTx = Transaction.fromHex(redeemTxHex)
+    const expectedRedeemScript = bscript.fromASM(
+      `OP_2 ${contract.publicKeyW.toString('hex')} ${contract.publicKeyB.toString('hex')} ${computer.wallet.publicKey.toString('hex')} OP_3 OP_CHECKMULTISIG`
+    )
+
+    // Validate and sign the transaction
+    const signedRedeemTx = ChessContractHelper.validateAndSignRedeemTx(
+      redeemTx,
+      Buffer.from(winnerPublicKey, 'hex'),
+      operatorKeyPair,
+      expectedRedeemScript,
+      NETWORKOBJ
+    )
+
+    // Broadcast the fully signed transaction
+    const finalTxId = await computer.broadcast(signedRedeemTx)
+    res.status(200).json(finalTxId)
+    return
   } catch (error) {
-    if (error instanceof Error)
-      res.status(500).json({ error: `Internal server error: ${error.message}` })
+    console.error(error)
+    res.status(500).json({ message: 'Internal server error' })
   }
 })
 
