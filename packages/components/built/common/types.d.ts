2adb3565a31424cd5f0e188715963b7110d6a76b
Tomás Díaz <tomas@bsv.direct>
2025-04-26 22:18:31 +0200

    updating for GRS and adding missing deps and fixes to build

--- packages/components/built/common/types.d.ts
+++ packages/components/built/common/types.d.ts
@@ -1,3 +1,3 @@
-export type Chain = 'LTC' | 'BTC' | 'DOGE' | 'PEPE';
+export type Chain = 'LTC' | 'BTC' | 'DOGE' | 'PEPE' | 'GRS';
 export type Network = 'testnet' | 'mainnet' | 'regtest';
 export type ModuleStorageType = 'taproot' | 'multisig';
