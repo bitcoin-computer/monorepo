5e45df3088da033abb2d5868cad8401a6fec2d57
jonty007 <vivek.singh2994@gmail.com>
2025-05-12 18:47:57 +0530

    remove API_BASE_URL as its not required

--- packages/chess-app/src/constants/modSpecs.ts
+++ packages/chess-app/src/constants/modSpecs.ts
@@ -2,31 +2,25 @@
 
 import 'dotenv/config'
 
-let API_BASE_URL: string
 let VITE_CHESS_GAME_MOD_SPEC: string
 let VITE_CHESS_USER_MOD_SPEC: string
 
 if (
   typeof import.meta !== 'undefined' &&
-  import.meta.env?.VITE_API_BASE_URL &&
   import.meta.env?.VITE_CHESS_GAME_MOD_SPEC &&
   import.meta.env?.VITE_CHESS_USER_MOD_SPEC
 ) {
   // Vite environment
-  API_BASE_URL = import.meta.env.VITE_API_BASE_URL
   VITE_CHESS_GAME_MOD_SPEC = import.meta.env.VITE_CHESS_GAME_MOD_SPEC
   VITE_CHESS_USER_MOD_SPEC = import.meta.env.VITE_CHESS_USER_MOD_SPEC
 } else {
   // Node.js environment
-  if (!process.env.VITE_API_BASE_URL)
-    throw new Error('VITE_API_BASE_URL is not defined in the .env file.')
   if (!process.env.VITE_CHESS_GAME_MOD_SPEC)
     throw new Error('VITE_CHESS_GAME_MOD_SPEC is not defined in the .env file.')
   if (!process.env.VITE_CHESS_USER_MOD_SPEC)
     throw new Error('VITE_CHESS_USER_MOD_SPEC is not defined in the .env file.')
-  API_BASE_URL = process.env.VITE_API_BASE_URL
   VITE_CHESS_GAME_MOD_SPEC = process.env.VITE_CHESS_GAME_MOD_SPEC
   VITE_CHESS_USER_MOD_SPEC = process.env.VITE_CHESS_USER_MOD_SPEC
 }
 
-export { API_BASE_URL, VITE_CHESS_GAME_MOD_SPEC, VITE_CHESS_USER_MOD_SPEC }
+export { VITE_CHESS_GAME_MOD_SPEC, VITE_CHESS_USER_MOD_SPEC }
