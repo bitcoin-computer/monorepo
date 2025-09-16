f73f60f0754c2fd6a4167f000c44c9fac6477f45
jonty007 <vivek.singh2994@gmail.com>
2025-09-16 13:34:30 +0530

    eslint fixes

--- packages/TBC20/test/token.test.ts
+++ packages/TBC20/test/token.test.ts
@@ -167,7 +167,7 @@ describe('TokenHelper', () => {
     })
 
     it('Should mint a root token', async () => {
-      const rootToken: any = await sender.sync(root)
+      const rootToken = (await sender.sync(root)) as Token
       expect(rootToken).not.to.be.undefined
       expect(rootToken._id).to.eq(root)
       expect(rootToken._rev).to.eq(root)
--- packages/swap/eslint.config.js
+++ packages/swap/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import chaiFriendly from 'eslint-plugin-chai-friendly'
 
 export default tseslint.config(
   { ignores: ['dist', 'build'] },
@@ -11,12 +12,14 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'chai-friendly': chaiFriendly,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      // remove them later
+      'no-unused-expressions': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
+      'chai-friendly/no-unused-expressions': 'error',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
