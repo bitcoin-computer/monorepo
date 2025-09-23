2649b2feb389f5eba280c63867c47fd9f2fd57fd
ltardivo <lauratardivo@gmail.com>
2025-09-23 14:05:03 -0700

    Adds more test to get-txos.test.ts

--- packages/docs/Lib/Computer/getTxos.md
+++ packages/docs/Lib/Computer/getTxos.md
@@ -43,12 +43,12 @@ An object with the query parameters.
 | verbosity | 0 for revision strings, 1 for rows from the Output table |
 | rev | Return TXOs with this exact revision |
 | address | Return TXOs that belong to this address. The address is computed from the output script (see below) |
-| satoshis | Return TXOs with this exact satoshi amount |
+| satoshis | Return TXOs with this exact satoshis amount |
 | asm | Return TXOs matching the asm script |
 | isObject | Return TXOs that are (or are not) smart objects |
 | mod | Return TXOs that are (or are not) smart objects created with this module or a descendant of it |
 | previous | Return TXOs whose previous is the provided parameter |
-| expHash | Return TXOs that are matching the provided expression hash |
+| expHash | Giving an expression, return TXOs that matches the hash of that expression |
 | blockHash | Return TXOs that are (or are not) included in the block with this hash |
 | spent | Return TXOs that are (or are not) spent |
 | publicKey | Return TXOs that are (or are not) owned by this public key |
@@ -76,6 +76,8 @@ asm = script.toASM(script)
 Then, queries can be made based on these computed columns. When filtering by address, only TXOs whose output script can be successfully converted to that address are considered. When filtering by public key, only TXOs whose output script includes the provided public key are considered. See the examples below for more details.
 
 To query all on chain objects, use the parameter `isObject: true`. It is a good practice to always combine this with the `publicKey` parameter to return only the objects owned by a specific public key, and to limit the scope of the query.
+
+To return only objects created with a specific module or its descendants, you can use the `mod` parameter. This is useful when you want to filter objects based on their module of origin. Within the Bitcoin Computer, the module is inherited to all descendant objects created from the original module object, except when the execution creates new objects or when the module is explicitly changed within the expression. See the examples, some test cases illustrate this behavior for token transfers.
 
 ### Return Value
 
--- packages/lib/test/lib/computer/get-txos.test.ts
+++ packages/lib/test/lib/computer/get-txos.test.ts
@@ -1,10 +1,11 @@
 import { Computer } from '@bitcoin-computer/lib'
-import { chain, expect, network, url } from '../../utils'
+import { payments, script, Transaction } from '@bitcoin-computer/nakamotojs'
+import { chain, expect, network, sleep, url } from '../../utils'
 
 describe('getTXOs', () => {
   let computer: Computer
 
-  class C extends Contract {
+  class Counter extends Contract {
     x: number
     constructor() {
       super({ x: 0 })
@@ -13,147 +14,313 @@ describe('getTXOs', () => {
       this.x += 1
     }
   }
+  class Token extends Contract {
+    amount: bigint
+    constructor(to: string, amount: bigint) {
+      super({ _owners: [to], amount })
+    }
+
+    transfer(to: string, amount: bigint): Token {
+      if (this.amount >= amount) {
+        // Send partial amount in a new object
+        this.amount -= amount
+        return new Token(to, amount)
+      }
+      throw new Error('Insufficient funds')
+    }
+  }
+
+  class Payment extends Contract {
+    constructor(_satoshis: bigint) {
+      super({ _satoshis })
+    }
+    transfer(to: string) {
+      this._owners = [to]
+    }
+    setSatoshis(a: bigint) {
+      this._satoshis = a
+    }
+  }
 
   before('Fund computer', async () => {
     computer = new Computer({ chain, network, url })
-    await computer.faucet(1e8)
-    const c = await computer.new(C, [])
+    await computer.faucet(2e8)
+    const c = await computer.new(Counter, [])
     await c.inc()
   })
 
-  it('Should throw for an empty query string', async () => {
-    let error
-    try {
-      await computer.getTXOs({})
-    } catch (e: any) {
-      error = e
-    }
-    expect(error).to.be.instanceOf(Error)
-    expect(error.message).to.eq('At least one query parameter must be provided')
-  })
-
-  it('Should get TXOs by address and return an array of strings with the default verbosity', async () => {
-    const txos = await computer.getTXOs({ address: computer.getAddress() })
-    expect(txos.length).to.be.greaterThan(0)
-    txos.forEach((txo) => {
-      if (typeof txo !== 'string') throw new Error('Txo is not a string')
-      expect(txo.split(':').length).to.eq(2)
-      expect(txo.split(':')[0].length).to.eq(64)
+  describe('Error handling', () => {
+    it('Should throw for an empty query string', async () => {
+      let error
+      try {
+        await computer.getTXOs({})
+      } catch (e: any) {
+        error = e
+      }
+      expect(error).to.be.instanceOf(Error)
+      expect(error.message).to.eq('At least one query parameter must be provided')
     })
   })
+  describe('Get by rev', () => {
+    it('Should get TXOs by rev', async () => {
+      const c3 = await computer.new(Counter, [])
+      const txos = await computer.getTXOs({ rev: c3._rev })
+      expect(txos.length).to.eq(1)
+      expect(txos[0]).to.eq(c3._rev)
 
-  it('Should  get TXOs by address and return all attributes with verbosity 1', async () => {
-    const txos = await computer.getTXOs({ verbosity: 1, address: computer.getAddress() })
-    expect(txos.length).to.be.greaterThan(0)
-    txos.forEach((txo) => {
-      if (typeof txo === 'object' && txo !== null) {
-        expect(txo.address).to.eq(computer.getAddress())
-        expect(txo.satoshis).to.be.a('bigint')
-        expect(txo.rev).to.be.a('string')
-        expect(txo.asm).to.be.a('string')
-        expect(txo.isObject).to.be.a('boolean')
+      const txosJson = await computer.getTXOs({ rev: c3._rev, verbosity: 1 })
+      expect(txosJson.length).to.eq(1)
+      if (typeof txosJson[0] === 'object' && txosJson[0] !== null) {
+        expect(txosJson[0].rev).to.eq(c3._rev)
+        expect(txosJson[0].isObject).to.eq(true)
+        expect(txosJson[0].satoshis).to.be.a('bigint')
+        expect(txosJson[0].asm).to.be.a('string')
       } else {
         throw new Error('Txo is not an object')
       }
     })
   })
-
-  it('Should get TXOs by address and return unpent txos only with spent=false', async () => {
-    const allTxos = await computer.getTXOs({ address: computer.getAddress() })
-    const unspentTxos = await computer.getTXOs({
-      address: computer.getAddress(),
-      spent: false,
+  describe('Get by address', () => {
+    it('Should get TXOs by address and return an array of strings with the default verbosity', async () => {
+      const txos = await computer.getTXOs({ address: computer.getAddress() })
+      expect(txos.length).to.be.greaterThan(0)
+      txos.forEach((txo) => {
+        if (typeof txo !== 'string') throw new Error('Txo is not a string')
+        expect(txo.split(':').length).to.eq(2)
+        expect(txo.split(':')[0].length).to.eq(64)
+      })
     })
-    unspentTxos.forEach((txo) => {
-      expect(allTxos).to.include(txo)
+
+    it('Should  get TXOs by address and return all attributes with verbosity 1', async () => {
+      const txos = await computer.getTXOs({ verbosity: 1, address: computer.getAddress() })
+      expect(txos.length).to.be.greaterThan(0)
+      txos.forEach((txo) => {
+        if (typeof txo === 'object' && txo !== null) {
+          expect(txo.address).to.eq(computer.getAddress())
+          expect(txo.satoshis).to.be.a('bigint')
+          expect(txo.rev).to.be.a('string')
+          expect(txo.asm).to.be.a('string')
+          expect(txo.isObject).to.be.a('boolean')
+        } else {
+          throw new Error('Txo is not an object')
+        }
+      })
+    })
+
+    it('Should get TXOs by address and return unpent txos only with spent=false', async () => {
+      const allTxos = await computer.getTXOs({ address: computer.getAddress() })
+      const unspentTxos = await computer.getTXOs({
+        address: computer.getAddress(),
+        spent: false,
+      })
+      unspentTxos.forEach((txo) => {
+        expect(allTxos).to.include(txo)
+      })
+    })
+
+    it('Should get TXOs by address and compute the balance for the computer address', async () => {
+      const txos = await computer.getTXOs({
+        address: computer.getAddress(),
+        spent: false,
+        verbosity: 1,
+      })
+      const balance: bigint = txos.reduce((acc: bigint, txo) => {
+        if (typeof txo === 'object' && txo !== null) {
+          return acc + txo.satoshis
+        } else {
+          throw new Error('Txo is not an object')
+        }
+      }, 0n)
+
+      const computerBalance = await computer.getBalance()
+      expect(balance).to.be.a('bigint')
+      expect(balance).to.be.greaterThan(0)
+      expect(balance).to.eq(computerBalance.balance)
+    })
+
+    it('Should get TXOs by address and omit on chain objects', async () => {
+      const c2 = await computer.new(Counter, [])
+      const txos = await computer.getTXOs({
+        address: computer.getAddress(),
+      })
+      expect(txos.length).to.be.greaterThan(0)
+      // On chain objects do not belong to the address query
+      expect(txos).to.not.include(c2._rev)
     })
   })
+  describe('Get by satoshis', () => {
+    it('Should get TXOs by satoshis amount', async () => {
+      const c1 = await computer.new(Payment, [1000000n])
+      const satoshis = 2_000_000n
+      await c1.setSatoshis(satoshis)
 
-  it('Should get TXOs by address and compute the balance for the computer address', async () => {
-    const txos = await computer.getTXOs({
-      address: computer.getAddress(),
-      spent: false,
-      verbosity: 1,
+      const txos = await computer.getTXOs({ satoshis })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.not.include(c1._id)
+      expect(txos).to.include(c1._rev)
     })
-    const balance: bigint = txos.reduce((acc: bigint, txo) => {
-      if (typeof txo === 'object' && txo !== null) {
-        return acc + txo.satoshis
-      } else {
-        throw new Error('Txo is not an object')
+  })
+  describe('Get by asm', () => {
+    it('Should get TXOs by asm substring', async () => {
+      class True extends Contract {
+        constructor() {
+          super({ _owners: 'OP_TRUE' })
+        }
       }
-    }, 0n)
+      const c2 = await computer.new(True, [])
 
-    const computerBalance = await computer.getBalance()
-    expect(balance).to.be.a('bigint')
-    expect(balance).to.be.greaterThan(0)
-    expect(balance).to.eq(computerBalance.balance)
-  })
-
-  it('Should get TXOs by address and omit on chain objects', async () => {
-    const c2 = await computer.new(C, [])
-    const txos = await computer.getTXOs({
-      address: computer.getAddress(),
+      const txos = await computer.getTXOs({ asm: 'OP_TRUE' })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(c2._rev)
     })
-    expect(txos.length).to.be.greaterThan(0)
-    // On chain objects do not belong to the address query
-    expect(txos).to.not.include(c2._rev)
   })
+  describe('Get by mod', () => {
+    it('Should get TXOs by mod for creation and object function calls', async () => {
+      const m = await computer.deploy('')
+      const c4 = await computer.new(Counter, [], m)
+      await c4.inc()
 
-  it('Should get TXOs by public key and include on chain objects', async () => {
-    const c2 = await computer.new(C, [])
-    const txos = await computer.getTXOs({ publicKey: computer.getPublicKey() })
-    expect(txos.length).to.be.greaterThan(0)
-    // On chain objects belong to the computer's public key
-    expect(txos).includes(c2._rev)
-  })
-
-  it('Should get TXOs by isObject key', async () => {
-    const computer2 = new Computer({ chain, network, url })
-    await computer2.faucet(1e8)
-    const c2 = await computer2.new(C, [])
-    const objectTxos = await computer2.getTXOs({ isObject: true })
-    expect(objectTxos.length).to.be.greaterThan(0)
-    expect(objectTxos).to.include(c2._rev)
-
-    const nonObjectTxos = await computer2.getTXOs({ isObject: false })
-    expect(nonObjectTxos.length).to.be.greaterThan(0)
-    expect(nonObjectTxos).to.not.include(c2._rev)
-
-    // It is a good practice to combine isObject with publicKey to narrow down the search
-    const objectTxosByPubKey = await computer2.getTXOs({
-      isObject: true,
-      publicKey: computer2.getPublicKey(),
-    })
-    expect(objectTxosByPubKey.length).to.be.greaterThan(0)
-    expect(objectTxosByPubKey).to.include(c2._rev)
-
-    // Lets spend the on chain object to test isObject filter
-    await c2.inc()
-
-    const objectTxosAfterSpend = await computer2.getTXOs({
-      isObject: true,
-      publicKey: computer2.getPublicKey(),
+      const txos = await computer.getTXOs({ mod: m })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(c4._id)
+      expect(txos).to.include(c4._rev)
     })
 
-    expect(objectTxosAfterSpend.length).to.greaterThan(0)
-    const spent = objectTxosAfterSpend.filter((txo) => txo === c2._id)
-    expect(spent.length).to.eq(1)
-    const unspent = objectTxosAfterSpend.filter((txo) => txo === c2._rev)
-    expect(unspent.length).to.eq(1)
+    it('Should get TXOs by mod only for the object direct inheritance, but not token transfers', async () => {
+      const m = await computer.deploy(`export ${Token}`)
+      const t = await computer.new(Token, [computer.getPublicKey(), 100n], m)
+      const computer2 = new Computer({ chain, network, url })
+      const newToken: Token = await t.transfer(computer2.getPublicKey(), 40n)
+
+      const txos = await computer.getTXOs({ mod: m })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(t._id)
+      expect(txos).to.not.include(newToken._rev)
+    })
+
+    it('Should get TXOs by mod on token transfers with explicit mod parameter', async () => {
+      const m = await computer.deploy(`export ${Token}`)
+      const t = await computer.new(Token, [computer.getPublicKey(), 100n], m)
+      const computer2 = new Computer({ chain, network, url })
+      const transferTx = await computer.encode({
+        exp: `a.transfer('${computer2.getPublicKey()}', 40n)`,
+        env: { a: t._rev },
+        mod: m, // explicitly specifying the mod here
+      })
+      await computer.broadcast(transferTx.tx)
+
+      // sync to the new token
+      const newToken = (await computer.sync(`${transferTx.tx.getId()}:0`)) as Token
+
+      const txos = await computer.getTXOs({ mod: m })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(t._id)
+      expect(txos).to.include(newToken._rev)
+    })
   })
+  describe('Get by isObject', () => {
+    it('Should get TXOs by isObject key', async () => {
+      const computer2 = new Computer({ chain, network, url })
+      await computer2.faucet(1e8)
+      const c2 = await computer2.new(Counter, [])
+      const objectTxos = await computer2.getTXOs({ isObject: true })
+      expect(objectTxos.length).to.be.greaterThan(0)
+      expect(objectTxos).to.include(c2._rev)
 
-  it('Should get TXOs by rev', async () => {
-    const c3 = await computer.new(C, [])
-    const txos = await computer.getTXOs({ rev: c3._rev })
-    expect(txos.length).to.eq(1)
-    expect(txos[0]).to.eq(c3._rev)
+      const nonObjectTxos = await computer2.getTXOs({ isObject: false })
+      expect(nonObjectTxos.length).to.be.greaterThan(0)
+      expect(nonObjectTxos).to.not.include(c2._rev)
 
-    const txosJson = await computer.getTXOs({ rev: c3._rev, verbosity: 1 })
-    expect(txosJson.length).to.eq(1)
-    if (typeof txosJson[0] === 'object' && txosJson[0] !== null) {
-      expect(txosJson[0].rev).to.eq(c3._rev)
-    } else {
-      throw new Error('Txo is not an object')
-    }
+      // It is a good practice to combine isObject with publicKey to narrow down the search
+      const objectTxosByPubKey = await computer2.getTXOs({
+        isObject: true,
+        publicKey: computer2.getPublicKey(),
+      })
+      expect(objectTxosByPubKey.length).to.be.greaterThan(0)
+      expect(objectTxosByPubKey).to.include(c2._rev)
+
+      // Lets spend the on chain object to test isObject filter
+      await c2.inc()
+
+      const objectTxosAfterSpend = await computer2.getTXOs({
+        isObject: true,
+        publicKey: computer2.getPublicKey(),
+      })
+
+      expect(objectTxosAfterSpend.length).to.greaterThan(0)
+      const spent = objectTxosAfterSpend.filter((txo) => txo === c2._id)
+      expect(spent.length).to.eq(1)
+      const unspent = objectTxosAfterSpend.filter((txo) => txo === c2._rev)
+      expect(unspent.length).to.eq(1)
+    })
+  })
+  describe('Get by previous', () => {
+    it('Should get TXOs by previous', async () => {
+      const c2 = await computer.new(Counter, [])
+      await c2.inc()
+
+      const txos = await computer.getTXOs({ previous: c2._id })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(c2._rev)
+    })
+  })
+  describe('Get by expHash', () => {
+    it('Should get TXOs by expHash', async () => {
+      class C extends Contract {}
+
+      const exp = `${C} new ${C.name}()`
+      const { tx } = await computer.encode({ exp })
+      await computer.broadcast(tx)
+
+      const txos = await computer.getTXOs({ expHash: exp })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(`${tx.getId()}:0`)
+    })
+  })
+  describe('Get by blockHash', () => {
+    it('Should get TXOs by blockHash', async () => {
+      const c2 = await computer.new(Counter, [])
+      const computer2 = new Computer({ chain, network, url })
+
+      // mine a block to confirm the transaction
+      const blockHex = await computer.rpcCall('generateToAddress', `1 ${computer2.getAddress()}`)
+      await sleep(500)
+      const txos = await computer.getTXOs({ blockHash: blockHex.result[0] })
+      expect(txos.length).to.be.greaterThan(0)
+      expect(txos).to.include(c2._rev)
+    })
+  })
+  describe('Get by publicKey', () => {
+    it('Should get TXOs by public key and include on chain objects', async () => {
+      const c2 = await computer.new(Counter, [])
+      const txos = await computer.getTXOs({ publicKey: computer.getPublicKey() })
+      expect(txos.length).to.be.greaterThan(0)
+      // On chain objects belong to the computer's public key
+      expect(txos).includes(c2._rev)
+    })
+  })
+  describe('Get by limit, offset and order', () => {
+    it('Should get TXOs by limit, offset and order', async () => {
+      const txoIds: string[] = []
+      for (let i = 0; i < 5; i++) {
+        const c = await computer.new(Counter, [])
+        txoIds.push(c._rev)
+      }
+
+      const allTxos = await computer.getTXOs({
+        publicKey: computer.getPublicKey(),
+        order: 'DESC',
+      })
+      const limitedTxos = await computer.getTXOs({
+        publicKey: computer.getPublicKey(),
+        limit: 2,
+        offset: 1,
+        order: 'DESC',
+      })
+
+      expect(limitedTxos.length).to.eq(2)
+      expect(limitedTxos[0]).to.eq(allTxos[1])
+      expect(limitedTxos[1]).to.eq(allTxos[2])
+    })
   })
 })
