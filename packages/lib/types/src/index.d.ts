d2e1c451dadcc3cf7f60482ab49d8a6f1e9f2c47
jonty007 <vivek.singh2994@gmail.com>
2026-03-17 21:35:35 +0530

    vite template fix, new types for lib

--- packages/lib/types/src/index.d.ts
+++ packages/lib/types/src/index.d.ts
@@ -1,6 +1,9 @@
 import { Transaction } from './transaction.js';
 import { Computer } from './computer.js';
-import { Contract } from './contract.js';
+import { Contract as GlobalContract } from './contract.js';
 import { Mock } from './mock.js';
-export { Computer, Contract, Mock, Transaction };
+export { Computer, GlobalContract as Contract, Mock, Transaction };
 export type * from './types.js';
+declare global {
+    var Contract: typeof GlobalContract;
+}
