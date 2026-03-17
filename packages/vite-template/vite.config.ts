7a46c8ef0fb363ef287583340667cab6b4017f01
ltardivo <lauratardivo@gmail.com>
2026-03-17 11:53:09 -0700

    Fixes vitest.puppeteer.config.ts

--- packages/vite-template/vite.config.ts
+++ packages/vite-template/vite.config.ts
@@ -1,5 +1,4 @@
 /// <reference types="vite/client" />
-
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
