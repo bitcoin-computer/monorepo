6de3664bcba7af8cf0d189a71c42b321a209b069
Clemens Ley <ley.clemens@gmail.com>
2026-05-13 11:53:01 -0700

    tbc777: delete preliminary versions of tbc777 contract

--- packages/TBC777/build/src/index.js
+++
@@ -1,4 +0,0 @@
-export { TBC20, TBC20Helper } from './tbc20.js';
-export { Escrow as EscrowTBC777, TBC777 } from './tbc777.js';
-export { Escrow as EscrowTBC777M, TBC777M } from './tbc777m.js';
-export { Escrow as EscrowTBC777P, TBC777P } from './tbc777p.js';
