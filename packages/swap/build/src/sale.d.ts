11633a629e340f86447eb107b87d86e274bd9b7f
ltardivo <lauratardivo@gmail.com>
2026-05-14 11:45:23 -0700

    Upload bundles and build files

--- packages/swap/build/src/sale.d.ts
+++ packages/swap/build/src/sale.d.ts
@@ -1,20 +1,18 @@
-import { Buffer } from 'buffer'
-import type { Transaction as TransactionType } from '@bitcoin-computer/lib'
-import { Payment, PaymentMock } from './payment.js'
+import { Buffer } from 'buffer';
+import type { Transaction as TransactionType } from '@bitcoin-computer/lib';
+import { Payment, PaymentMock } from './payment.js';
 export declare class Sale extends Contract {
-  static exec(o: any, p: Payment): any[]
+    static exec(o: any, p: Payment): any[];
 }
 export declare class SaleHelper {
-  computer: any
-  mod?: string
-  constructor(computer: any, mod?: string)
-  deploy(): Promise<string>
-  createSaleTx(object: any, payment: PaymentMock): any
-  isSaleTx(tx: TransactionType): Promise<boolean>
-  checkSaleTx(tx: TransactionType): Promise<bigint>
-  static finalizeSaleTx(
-    tx: TransactionType,
-    payment: Payment,
-    scriptPubKey: Buffer,
-  ): TransactionType
+    computer: any;
+    mod?: string;
+    constructor(computer: any, mod?: string);
+    deploy(): Promise<string>;
+    createSaleTx(object: any, payment: PaymentMock): any;
+    isSaleTx(tx: TransactionType): Promise<boolean>;
+    checkSaleTx(tx: TransactionType): Promise<bigint>;
+    static finalizeSaleTx(tx: TransactionType, payment: {
+        _rev: string;
+    }, scriptPubKey: Buffer): TransactionType;
 }
