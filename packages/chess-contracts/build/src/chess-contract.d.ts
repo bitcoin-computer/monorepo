15007bf6d65adbdb2b5b5888159d55cd49017311
Clemens Ley <ley.clemens@gmail.com>
2026-05-19 10:36:58 -0700

    dist files

--- packages/chess-contracts/build/src/chess-contract.d.ts
+++ packages/chess-contracts/build/src/chess-contract.d.ts
@@ -1,6 +1,7 @@
 import { Computer, SmartContract } from '@bitcoin-computer/lib';
 export declare class ChessContract extends Contract {
     wagerAmount: bigint;
+    timeLimit: bigint;
     nameW: string;
     nameB: string;
     publicKeyW: string;
@@ -14,10 +15,25 @@ export declare class ChessContract extends Contract {
     root: string;
     tokenIdW: string;
     tokenIdB: string;
-    constructor(root: string, wagerAmount: bigint);
+    constructor(root: string, wagerAmount: bigint, timeLimit: bigint);
     acceptDeposit(token: any, amount: bigint, name: string, nextOwner: string): Promise<void>;
     move(from: string, to: string, promotion: string): boolean;
+    claimWin(): void;
+    resign(): void;
     isGameOver(): boolean;
+    hasTimedOutW(): Promise<boolean>;
+    hasTimedOutB(): Promise<boolean>;
+    /**
+     * Calculates white time (timeW) and black time (timeB) from a list of timestamps.
+     * timeW = (t2 - t1) + (t4 - t3) + (t6 - t5) + ...
+     * timeB = (t3 - t2) + (t5 - t4) + (t7 - t6) + ...
+     *
+     * Note: timeW + timeB will equal (tn - t1).
+     */
+    calculateTimes(): Promise<{
+        timeW: bigint;
+        timeB: bigint;
+    }>;
 }
 export declare class ChessContractHelper {
     computer: Computer;
