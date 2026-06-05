19ecd8cd6377748cc0cd592e8ecbcc3c37f2a32a
ltardivo <lauratardivo@gmail.com>
2026-06-05 11:06:53 -0700

    Uses prettier on type files in lib

--- packages/lib/types/src/index.d.ts
+++ packages/lib/types/src/index.d.ts
@@ -1,11 +1,10 @@
 import { Transaction } from './transaction.js'
 import { Computer } from './computer.js'
-import { Contract as RuntimeContract } from './contract.js'
+import { Contract } from './contract.js'
 import { Mock } from './mock.js'
 export { Computer, Mock, Transaction }
-export { RuntimeContract as Contract }
+export { Contract }
 export type { InnerComputer } from './contract-env.js'
 export { precise } from './types.js'
 export { lifted } from './types.js'
 export type * from './types.js'
-export * from './types.js'
